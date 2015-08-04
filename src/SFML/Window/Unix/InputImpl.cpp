SFML/SFML

Fix keypad keys not being detected on Linux

src/SFML/Window/Unix/InputImpl.cpp
@@ -137,16 +137,16 @@ namespace
         keycodeMap[sf::Keyboard::Right]     = getKeycode(XK_Right);
         keycodeMap[sf::Keyboard::Up]        = getKeycode(XK_Up);
         keycodeMap[sf::Keyboard::Down]      = getKeycode(XK_Down);
-        keycodeMap[sf::Keyboard::Numpad0]   = getKeycode(XK_KP_0);
-        keycodeMap[sf::Keyboard::Numpad1]   = getKeycode(XK_KP_1);
-        keycodeMap[sf::Keyboard::Numpad2]   = getKeycode(XK_KP_2);
-        keycodeMap[sf::Keyboard::Numpad3]   = getKeycode(XK_KP_3);
-        keycodeMap[sf::Keyboard::Numpad4]   = getKeycode(XK_KP_4);
-        keycodeMap[sf::Keyboard::Numpad5]   = getKeycode(XK_KP_5);
-        keycodeMap[sf::Keyboard::Numpad6]   = getKeycode(XK_KP_6);
-        keycodeMap[sf::Keyboard::Numpad7]   = getKeycode(XK_KP_7);
-        keycodeMap[sf::Keyboard::Numpad8]   = getKeycode(XK_KP_8);
-        keycodeMap[sf::Keyboard::Numpad9]   = getKeycode(XK_KP_9);
+        keycodeMap[sf::Keyboard::Numpad0]   = getKeycode(XK_KP_Insert);
+        keycodeMap[sf::Keyboard::Numpad1]   = getKeycode(XK_KP_End);
+        keycodeMap[sf::Keyboard::Numpad2]   = getKeycode(XK_KP_Down);
+        keycodeMap[sf::Keyboard::Numpad3]   = getKeycode(XK_KP_Page_Down);
+        keycodeMap[sf::Keyboard::Numpad4]   = getKeycode(XK_KP_Left);
+        keycodeMap[sf::Keyboard::Numpad5]   = getKeycode(XK_KP_Begin);
+        keycodeMap[sf::Keyboard::Numpad6]   = getKeycode(XK_KP_Right);
+        keycodeMap[sf::Keyboard::Numpad7]   = getKeycode(XK_KP_Home);
+        keycodeMap[sf::Keyboard::Numpad8]   = getKeycode(XK_KP_Up);
+        keycodeMap[sf::Keyboard::Numpad9]   = getKeycode(XK_KP_Page_Up);
         keycodeMap[sf::Keyboard::F1]        = getKeycode(XK_F1);
         keycodeMap[sf::Keyboard::F2]        = getKeycode(XK_F2);
         keycodeMap[sf::Keyboard::F3]        = getKeycode(XK_F3);

vittorioromeo/VRSFML

src/SFML/Window/Unix/InputImpl.cpp
@@ -346,16 +346,16 @@ void InputImpl::buildMap()
     keycodeMap[sf::Keyboard::X]         = getKeycode(XK_x);
     keycodeMap[sf::Keyboard::Y]         = getKeycode(XK_y);
     keycodeMap[sf::Keyboard::Z]         = getKeycode(XK_z);
-    keycodeMap[sf::Keyboard::Num0]      = getKeycode(XK_0);
-    keycodeMap[sf::Keyboard::Num1]      = getKeycode(XK_1);
-    keycodeMap[sf::Keyboard::Num2]      = getKeycode(XK_2);
-    keycodeMap[sf::Keyboard::Num3]      = getKeycode(XK_3);
-    keycodeMap[sf::Keyboard::Num4]      = getKeycode(XK_4);
-    keycodeMap[sf::Keyboard::Num5]      = getKeycode(XK_5);
-    keycodeMap[sf::Keyboard::Num6]      = getKeycode(XK_6);
-    keycodeMap[sf::Keyboard::Num7]      = getKeycode(XK_7);
-    keycodeMap[sf::Keyboard::Num8]      = getKeycode(XK_8);
-    keycodeMap[sf::Keyboard::Num9]      = getKeycode(XK_9);
+    keycodeMap[sf::Keyboard::Num0]      = getKeycode(XK_KP_Insert);
+    keycodeMap[sf::Keyboard::Num1]      = getKeycode(XK_KP_End);
+    keycodeMap[sf::Keyboard::Num2]      = getKeycode(XK_KP_Down);
+    keycodeMap[sf::Keyboard::Num3]      = getKeycode(XK_KP_Page_Down);
+    keycodeMap[sf::Keyboard::Num4]      = getKeycode(XK_KP_Left);
+    keycodeMap[sf::Keyboard::Num5]      = getKeycode(XK_KP_Begin);
+    keycodeMap[sf::Keyboard::Num6]      = getKeycode(XK_KP_Right);
+    keycodeMap[sf::Keyboard::Num7]      = getKeycode(XK_KP_Home);
+    keycodeMap[sf::Keyboard::Num8]      = getKeycode(XK_KP_Up);
+    keycodeMap[sf::Keyboard::Num9]      = getKeycode(XK_KP_Page_Up);
     keycodeMap[sf::Keyboard::Escape]    = getKeycode(XK_Escape);
     keycodeMap[sf::Keyboard::LControl]  = getKeycode(XK_Control_L);
     keycodeMap[sf::Keyboard::LShift]    = getKeycode(XK_Shift_L);