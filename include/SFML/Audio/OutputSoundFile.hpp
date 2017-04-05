SFML/SFML

Removed all manual memory management.

include/SFML/Audio/OutputSoundFile.hpp
@@ -31,6 +31,7 @@
 #include <SFML/Audio/Export.hpp>
 #include <SFML/System/NonCopyable.hpp>
 #include <string>
+#include <memory>
 
 
 namespace sf
@@ -93,7 +94,7 @@ private:
     ////////////////////////////////////////////////////////////
     // Member data
     ////////////////////////////////////////////////////////////
-    SoundFileWriter* m_writer; ///< Writer that handles I/O on the file's format
+    std::shared_ptr<SoundFileWriter> m_writer; ///< Writer that handles I/O on the file's format
 };
 
 } // namespace sf

include/SFML/System/ThreadLocal.hpp
@@ -31,6 +31,7 @@
 #include <SFML/System/Export.hpp>
 #include <SFML/System/NonCopyable.hpp>
 #include <cstdlib>
+#include <memory>
 
 
 namespace sf
@@ -83,7 +84,7 @@ private:
     ////////////////////////////////////////////////////////////
     // Member data
     ////////////////////////////////////////////////////////////
-    priv::ThreadLocalImpl* m_impl; ///< Pointer to the OS specific implementation
+    std::shared_ptr<priv::ThreadLocalImpl> m_impl; ///< Pointer to the OS specific implementation
 };
 
 } // namespace sf

include/SFML/Window/Context.hpp
@@ -32,6 +32,7 @@
 #include <SFML/Window/GlResource.hpp>
 #include <SFML/Window/ContextSettings.hpp>
 #include <SFML/System/NonCopyable.hpp>
+#include <memory>
 
 
 namespace sf
@@ -135,7 +136,7 @@ private:
     ////////////////////////////////////////////////////////////
     // Member data
     ////////////////////////////////////////////////////////////
-    priv::GlContext* m_context; ///< Internal OpenGL context
+    std::shared_ptr<priv::GlContext> m_context; ///< Internal OpenGL context
 };
 
 } // namespace sf

include/SFML/Window/Window.hpp
@@ -38,6 +38,7 @@
 #include <SFML/System/Vector2.hpp>
 #include <SFML/System/NonCopyable.hpp>
 #include <SFML/System/String.hpp>
+#include <memory>
 
 
 namespace sf
@@ -522,11 +523,11 @@ private:
     ////////////////////////////////////////////////////////////
     // Member data
     ////////////////////////////////////////////////////////////
-    priv::WindowImpl* m_impl;           ///< Platform-specific implementation of the window
-    priv::GlContext*  m_context;        ///< Platform-specific implementation of the OpenGL context
-    Clock             m_clock;          ///< Clock for measuring the elapsed time between frames
-    Time              m_frameTimeLimit; ///< Current framerate limit
-    Vector2u          m_size;           ///< Current size of the window
+    std::shared_ptr<priv::WindowImpl> m_impl;   ///< Platform-specific implementation of the window
+    std::shared_ptr<priv::GlContext> m_context; ///< Platform-specific implementation of the OpenGL context
+    Clock             m_clock;                  ///< Clock for measuring the elapsed time between frames
+    Time              m_frameTimeLimit;         ///< Current framerate limit
+    Vector2u          m_size;                   ///< Current size of the window
 };
 
 } // namespace sf

src/SFML/Audio/SoundFileFactory.cpp
@@ -63,7 +63,7 @@ SoundFileFactory::WriterFactoryArray SoundFileFactory::s_writers;
 
 
 ////////////////////////////////////////////////////////////
-SoundFileReader* SoundFileFactory::createReaderFromFilename(const std::string& filename)
+std::shared_ptr<SoundFileReader> SoundFileFactory::createReaderFromFilename(const std::string& filename)
 {
     // Register the built-in readers/writers on first call
     ensureDefaultReadersWritersRegistered();
@@ -90,7 +90,7 @@ SoundFileReader* SoundFileFactory::createReaderFromFilename(const std::string& f
 
 
 ////////////////////////////////////////////////////////////
-SoundFileReader* SoundFileFactory::createReaderFromMemory(const void* data, std::size_t sizeInBytes)
+std::shared_ptr<SoundFileReader> SoundFileFactory::createReaderFromMemory(const void* data, std::size_t sizeInBytes)
 {
     // Register the built-in readers/writers on first call
     ensureDefaultReadersWritersRegistered();
@@ -114,7 +114,7 @@ SoundFileReader* SoundFileFactory::createReaderFromMemory(const void* data, std:
 
 
 ////////////////////////////////////////////////////////////
-SoundFileReader* SoundFileFactory::createReaderFromStream(InputStream& stream)
+std::shared_ptr<SoundFileReader> SoundFileFactory::createReaderFromStream(InputStream& stream)
 {
     // Register the built-in readers/writers on first call
     ensureDefaultReadersWritersRegistered();
@@ -134,7 +134,7 @@ SoundFileReader* SoundFileFactory::createReaderFromStream(InputStream& stream)
 
 
 ////////////////////////////////////////////////////////////
-SoundFileWriter* SoundFileFactory::createWriterFromFilename(const std::string& filename)
+std::shared_ptr<SoundFileWriter> SoundFileFactory::createWriterFromFilename(const std::string& filename)
 {
     // Register the built-in readers/writers on first call
     ensureDefaultReadersWritersRegistered();

src/SFML/System/ThreadLocal.cpp
@@ -39,16 +39,13 @@ namespace sf
 ////////////////////////////////////////////////////////////
 ThreadLocal::ThreadLocal(void* value)
 {
-    m_impl = new priv::ThreadLocalImpl;
+    m_impl = std::make_shared<priv::ThreadLocalImpl>();
     setValue(value);
 }
 
 
 ////////////////////////////////////////////////////////////
-ThreadLocal::~ThreadLocal()
-{
-    delete m_impl;
-}
+ThreadLocal::~ThreadLocal() = default;
 
 
 ////////////////////////////////////////////////////////////

vittorioromeo/VRSFML

include/SFML/Audio/OutputSoundFile.hpp
@@ -31,6 +31,7 @@
 #include <SFML/Audio/Export.hpp>
 #include <SFML/System/NonCopyable.hpp>
 #include <string>
+#include <memory>
 
 
 namespace sf
@@ -93,7 +94,7 @@ private:
     ////////////////////////////////////////////////////////////
     // Member data
     ////////////////////////////////////////////////////////////
-    SoundFileWriter* m_writer; ///< Writer that handles I/O on the file's format
+    std::unique_ptr<SoundFileWriter> m_writer; ///< Writer that handles I/O on the file's format
 };
 
 } // namespace sf

include/SFML/System/ThreadLocal.hpp
@@ -31,6 +31,7 @@
 #include <SFML/System/Export.hpp>
 #include <SFML/System/NonCopyable.hpp>
 #include <cstdlib>
+#include <memory>
 
 
 namespace sf
@@ -83,7 +84,7 @@ private:
     ////////////////////////////////////////////////////////////
     // Member data
     ////////////////////////////////////////////////////////////
-    priv::ThreadLocalImpl* m_impl; ///< Pointer to the OS specific implementation
+    std::unique_ptr<priv::ThreadLocalImpl> m_impl; ///< Pointer to the OS specific implementation
 };
 
 } // namespace sf

include/SFML/Window/Context.hpp
@@ -32,6 +32,7 @@
 #include <SFML/Window/GlResource.hpp>
 #include <SFML/Window/ContextSettings.hpp>
 #include <SFML/System/NonCopyable.hpp>
+#include <memory>
 
 
 namespace sf
@@ -135,7 +136,7 @@ private:
     ////////////////////////////////////////////////////////////
     // Member data
     ////////////////////////////////////////////////////////////
-    priv::GlContext* m_context; ///< Internal OpenGL context
+    std::unique_ptr<priv::GlContext> m_context; ///< Internal OpenGL context
 };
 
 } // namespace sf

include/SFML/Window/Window.hpp
@@ -38,6 +38,7 @@
 #include <SFML/System/Vector2.hpp>
 #include <SFML/System/NonCopyable.hpp>
 #include <SFML/System/String.hpp>
+#include <memory>
 
 
 namespace sf
@@ -522,11 +523,11 @@ private:
     ////////////////////////////////////////////////////////////
     // Member data
     ////////////////////////////////////////////////////////////
-    priv::WindowImpl* m_impl;           ///< Platform-specific implementation of the window
-    priv::GlContext*  m_context;        ///< Platform-specific implementation of the OpenGL context
-    Clock             m_clock;          ///< Clock for measuring the elapsed time between frames
-    Time              m_frameTimeLimit; ///< Current framerate limit
-    Vector2u          m_size;           ///< Current size of the window
+    std::unique_ptr<priv::WindowImpl> m_impl;   ///< Platform-specific implementation of the window
+    std::unique_ptr<priv::GlContext> m_context; ///< Platform-specific implementation of the OpenGL context
+    Clock             m_clock;                  ///< Clock for measuring the elapsed time between frames
+    Time              m_frameTimeLimit;         ///< Current framerate limit
+    Vector2u          m_size;                   ///< Current size of the window
 };
 
 } // namespace sf

src/SFML/Audio/SoundFileFactory.cpp
@@ -63,7 +63,7 @@ SoundFileFactory::WriterFactoryArray SoundFileFactory::s_writers;
 
 
 ////////////////////////////////////////////////////////////
-SoundFileReader* SoundFileFactory::createReaderFromFilename(const std::string& filename)
+std::unique_ptr<SoundFileReader> SoundFileFactory::createReaderFromFilename(const std::string& filename)
 {
     // Register the built-in readers/writers on first call
     ensureDefaultReadersWritersRegistered();
@@ -90,7 +90,7 @@ SoundFileReader* SoundFileFactory::createReaderFromFilename(const std::string& f
 
 
 ////////////////////////////////////////////////////////////
-SoundFileReader* SoundFileFactory::createReaderFromMemory(const void* data, std::size_t sizeInBytes)
+std::unique_ptr<SoundFileReader> SoundFileFactory::createReaderFromMemory(const void* data, std::size_t sizeInBytes)
 {
     // Register the built-in readers/writers on first call
     ensureDefaultReadersWritersRegistered();
@@ -114,7 +114,7 @@ SoundFileReader* SoundFileFactory::createReaderFromMemory(const void* data, std:
 
 
 ////////////////////////////////////////////////////////////
-SoundFileReader* SoundFileFactory::createReaderFromStream(InputStream& stream)
+std::unique_ptr<SoundFileReader> SoundFileFactory::createReaderFromStream(InputStream& stream)
 {
     // Register the built-in readers/writers on first call
     ensureDefaultReadersWritersRegistered();
@@ -134,7 +134,7 @@ SoundFileReader* SoundFileFactory::createReaderFromStream(InputStream& stream)
 
 
 ////////////////////////////////////////////////////////////
-SoundFileWriter* SoundFileFactory::createWriterFromFilename(const std::string& filename)
+std::unique_ptr<SoundFileWriter> SoundFileFactory::createWriterFromFilename(const std::string& filename)
 {
     // Register the built-in readers/writers on first call
     ensureDefaultReadersWritersRegistered();

src/SFML/System/ThreadLocal.cpp
@@ -39,16 +39,13 @@ namespace sf
 ////////////////////////////////////////////////////////////
 ThreadLocal::ThreadLocal(void* value)
 {
-    m_impl = new priv::ThreadLocalImpl;
+    m_impl = std::make_unique<priv::ThreadLocalImpl>();
     setValue(value);
 }
 
 
 ////////////////////////////////////////////////////////////
-ThreadLocal::~ThreadLocal()
-{
-    delete m_impl;
-}
+ThreadLocal::~ThreadLocal() = default;
 
 
 ////////////////////////////////////////////////////////////