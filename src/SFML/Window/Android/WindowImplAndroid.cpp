rroohhh/SFML

Rewrote Cursor with new API and OS X implementation

src/SFML/Window/Android/WindowImplAndroid.cpp
@@ -186,14 +186,7 @@ void WindowImplAndroid::setMouseCursorGrabbed(bool grabbed)
 
 
 ////////////////////////////////////////////////////////////
-void WindowImplAndroid::setMouseCursor(Window::Cursor cursor)
-{
-    // Not applicable
-}
-
-
-////////////////////////////////////////////////////////////
-void WindowImplAndroid::setMouseCursor(const Uint8* pixels, unsigned int width, unsigned int height, Uint16 hotspotX, Uint16 hotspotY)
+void WindowImplAndroid::setMouseCursor(const Cursor& cursor)
 {
     // Not applicable
 }

vittorioromeo/VRSFML

src/SFML/Window/Android/WindowImplAndroid.cpp
@@ -186,14 +186,7 @@ void WindowImplAndroid::setMouseCursorGrabbed(bool grabbed)
 
 
 ////////////////////////////////////////////////////////////
-void WindowImplAndroid::setMouseCursor(Window::Cursor cursor)
-{
-    // Not applicable
-}
-
-
-////////////////////////////////////////////////////////////
-void WindowImplAndroid::setMouseCursor(const Uint8* pixels, unsigned int width, unsigned int height, Uint16 hotspotX, Uint16 hotspotY)
+void WindowImplAndroid::setMouseCursor(const CursorImpl& cursor)
 {
     // Not applicable
 }