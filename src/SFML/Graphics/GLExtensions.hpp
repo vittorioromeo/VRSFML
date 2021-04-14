vittorioromeo/VRSFML

commit 3c755ae09e9c0da074e525a123868333bf175a85

Updated glad loader to OpenGL 4.6 Compatibility Profile as base and added version support defines to GLExtensions.hpp.

src/SFML/Graphics/GLExtensions.hpp
@@ -308,6 +308,27 @@
     #define GLEXT_geometry_shader4                    SF_GLAD_GL_ARB_geometry_shader4
     #define GLEXT_GL_GEOMETRY_SHADER                  GL_GEOMETRY_SHADER_ARB
 
+    // OpenGL Versions
+    #define GLEXT_GL_VERSION_1_0                      SF_GLAD_GL_VERSION_1_0
+    #define GLEXT_GL_VERSION_1_1                      SF_GLAD_GL_VERSION_1_1
+    #define GLEXT_GL_VERSION_1_2                      SF_GLAD_GL_VERSION_1_2
+    #define GLEXT_GL_VERSION_1_3                      SF_GLAD_GL_VERSION_1_3
+    #define GLEXT_GL_VERSION_1_4                      SF_GLAD_GL_VERSION_1_4
+    #define GLEXT_GL_VERSION_1_5                      SF_GLAD_GL_VERSION_1_5
+    #define GLEXT_GL_VERSION_2_0                      SF_GLAD_GL_VERSION_2_0
+    #define GLEXT_GL_VERSION_2_1                      SF_GLAD_GL_VERSION_2_1
+    #define GLEXT_GL_VERSION_3_0                      SF_GLAD_GL_VERSION_3_0
+    #define GLEXT_GL_VERSION_3_1                      SF_GLAD_GL_VERSION_3_1
+    #define GLEXT_GL_VERSION_3_2                      SF_GLAD_GL_VERSION_3_2
+    #define GLEXT_GL_VERSION_3_3                      SF_GLAD_GL_VERSION_3_3
+    #define GLEXT_GL_VERSION_4_0                      SF_GLAD_GL_VERSION_4_0
+    #define GLEXT_GL_VERSION_4_1                      SF_GLAD_GL_VERSION_4_1
+    #define GLEXT_GL_VERSION_4_2                      SF_GLAD_GL_VERSION_4_2
+    #define GLEXT_GL_VERSION_4_3                      SF_GLAD_GL_VERSION_4_3
+    #define GLEXT_GL_VERSION_4_4                      SF_GLAD_GL_VERSION_4_4
+    #define GLEXT_GL_VERSION_4_5                      SF_GLAD_GL_VERSION_4_5
+    #define GLEXT_GL_VERSION_4_6                      SF_GLAD_GL_VERSION_4_6
+
 #endif
 
 namespace sf

commit ced4acbb0d5f7a96bbb0fbcde62f642c39a89966

Fixed regression introduced in 3c755ae causing EXT_texture_edge_clamp support to not be considered, added explicit initialization of nbFormats to 0 in WglContext.cpp in order to prevent broken graphics drivers from filling nbFormats with a garbage value.
Fixes #1786.

src/SFML/Graphics/GLExtensions.hpp
@@ -308,6 +308,8 @@
     #define GLEXT_geometry_shader4                    SF_GLAD_GL_ARB_geometry_shader4
     #define GLEXT_GL_GEOMETRY_SHADER                  GL_GEOMETRY_SHADER_ARB
 
+#endif
+
     // OpenGL Versions
     #define GLEXT_GL_VERSION_1_0                      SF_GLAD_GL_VERSION_1_0
     #define GLEXT_GL_VERSION_1_1                      SF_GLAD_GL_VERSION_1_1
@@ -328,8 +330,6 @@
     #define GLEXT_GL_VERSION_4_4                      SF_GLAD_GL_VERSION_4_4
     #define GLEXT_GL_VERSION_4_5                      SF_GLAD_GL_VERSION_4_5
     #define GLEXT_GL_VERSION_4_6                      SF_GLAD_GL_VERSION_4_6
-
-#endif
 
 namespace sf
 {

src/SFML/Graphics/Texture.cpp
@@ -162,7 +162,7 @@ bool Texture::create(unsigned int width, unsigned int height)
     // Make sure that the current texture binding will be preserved
     priv::TextureSaver save;
 
-    static bool textureEdgeClamp = GLEXT_texture_edge_clamp;
+    static bool textureEdgeClamp = GLEXT_texture_edge_clamp || GLEXT_GL_VERSION_1_2 || Context::isExtensionAvailable("GL_EXT_texture_edge_clamp");
 
     if (!m_isRepeated && !textureEdgeClamp)
     {

src/SFML/Window/Win32/WglContext.cpp
@@ -295,7 +295,7 @@ int WglContext::selectBestPixelFormat(HDC deviceContext, unsigned int bitsPerPix
 
         // Let's check how many formats are supporting our requirements
         int  formats[512];
-        UINT nbFormats;
+        UINT nbFormats = 0; // We must initialize to 0 otherwise broken drivers might fill with garbage in the following call
         bool isValid = wglChoosePixelFormatARB(deviceContext, intAttributes, NULL, 512, formats, &nbFormats) != FALSE;
 
         if (!isValid)