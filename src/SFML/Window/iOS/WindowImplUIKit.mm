xlin318/SFML

Rewrite `sf::Event` API to improve type safety
This new API is built on top of std::variant. This allows us to
store many different event types in a space-efficient way and access
the active event type in a type-safe manner that eliminates the
categories of UB that are possible with unions.

Co-authored-by: kimci86 <kimci86@hotmail.fr>

src/SFML/Window/iOS/WindowImplUIKit.mm
@@ -226,9 +226,9 @@ bool WindowImplUIKit::hasFocus() const
 ////////////////////////////////////////////////////////////
 void WindowImplUIKit::forwardEvent(Event event)
 {
-    if (event.type == Event::GainedFocus)
+    if (event.is<Event::GainedFocus>())
         m_hasFocus = true;
-    else if (event.type == Event::LostFocus)
+    else if (event.is<Event::LostFocus>())
         m_hasFocus = false;
 
     pushEvent(event);

vittorioromeo/VRSFML

src/SFML/Window/iOS/WindowImplUIKit.mm
@@ -230,9 +230,9 @@ bool WindowImplUIKit::hasFocus() const
 ////////////////////////////////////////////////////////////
 void WindowImplUIKit::forwardEvent(Event event)
 {
-    if (event.type == Event::GainedFocus)
+    if (event.is<Event::FocusGained>())
         m_hasFocus = true;
-    else if (event.type == Event::LostFocus)
+    else if (event.is<Event::FocusLost>())
         m_hasFocus = false;
 
     pushEvent(event);