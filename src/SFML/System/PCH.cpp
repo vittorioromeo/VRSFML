ef98895f3e7d02f9ce42b6cfca5693a7e2018880
vittorioromeo <mail@vittorioromeo.com>
2024-06-29 19:37:14 +0200

    Implement PCH support with reuse for Doctest

--- src/SFML/System/PCH.cpp
+++ src/SFML/System/PCH.cpp
@@ -22,51 +22,15 @@
 //
 ////////////////////////////////////////////////////////////
 
-#pragma once
-
 #ifndef SFML_ENABLE_PCH
-#error "PCH header included, but `SFML_ENABLE_PCH` was not defined"
+#error "PCH source included, but `SFML_ENABLE_PCH` was not defined"
 #else // SFML_ENABLE_PCH
 
-////////////////////////////////////////////////////////////
-// Precompiled Headers
-////////////////////////////////////////////////////////////
-
-#include <SFML/Config.hpp>
-
-#ifdef SFML_SYSTEM_WINDOWS
-
-#include <SFML/System/Win32/WindowsHeader.hpp>
-
-#include <dinput.h>
-#include <mmsystem.h>
-
-#endif // SFML_SYSTEM_WINDOWS
-
-#include <SFML/System/Err.hpp>
-#include <SFML/System/String.hpp>
-#include <SFML/System/Time.hpp>
-#include <SFML/System/UniquePtr.hpp>
-#include <SFML/System/Vector2.hpp>
-
-#include <algorithm>
-#include <chrono>
-#include <filesystem>
-#include <memory>
-#include <mutex>
-#include <optional>
-#include <string>
-#include <unordered_map>
-#include <vector>
-
-#include <cassert>
-#include <cmath>
-#include <cstdint>
-#include <cstdlib>
-#include <cstring>
-
 #ifdef SFML_BUILD_TEST_SUITE
-#include <doctest/parts/doctest_fwd.h>
+
+#define DOCTEST_CONFIG_IMPLEMENT 1
+#include <doctest/doctest.h>
+
 #endif // SFML_BUILD_TEST_SUITE
 
 #endif // SFML_ENABLE_PCH
