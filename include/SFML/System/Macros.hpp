6b8cb132ac26a8ee7970fd9cdb34a618a6b18314
vittorioromeo <mail@vittorioromeo.com>
2024-07-01 01:29:19 +0200

    Add missing `Macros.hpp` header

---
+++ include/SFML/System/Macros.hpp
@@ -0,0 +1,51 @@
+////////////////////////////////////////////////////////////
+//
+// SFML - Simple and Fast Multimedia Library
+// Copyright (C) 2007-2024 Laurent Gomila (laurent@sfml-dev.org)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+//    you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment
+//    in the product documentation would be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such,
+//    and must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+////////////////////////////////////////////////////////////
+
+#pragma once
+
+namespace sf::priv
+{
+
+template <typename T>
+struct RemoveRef
+{
+    using type = T;
+};
+
+template <typename T>
+struct RemoveRef<T&>
+{
+    using type = T;
+};
+
+template <typename T>
+struct RemoveRef<T&&>
+{
+    using type = T;
+};
+
+} // namespace sf::priv
+
+#define SFML_MOVE(...)    static_cast<typename ::sf::priv::RemoveRef<decltype(__VA_ARGS__)>::type&&>(__VA_ARGS__)
+#define SFML_FORWARD(...) static_cast<decltype(__VA_ARGS__)&&>(__VA_ARGS__)
