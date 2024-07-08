vittorioromeo/VRSFML

commit 4acd2779efd542da86e4c864954c0e2599d2d088

Move fullscreen window bookkeeping to a lower level

include/SFML/Window/WindowBase.hpp
@@ -513,22 +513,6 @@ private:
     void initialize();
 
     ////////////////////////////////////////////////////////////
-    /// \brief Get the fullscreen window
-    ///
-    /// \return The fullscreen window or a null pointer if there is none
-    ///
-    ////////////////////////////////////////////////////////////
-    const WindowBase* getFullscreenWindow();
-
-    ////////////////////////////////////////////////////////////
-    /// \brief Set a window as the fullscreen window
-    ///
-    /// \param window Window to set as fullscreen window
-    ///
-    ////////////////////////////////////////////////////////////
-    void setFullscreenWindow(const WindowBase* window);
-
-    ////////////////////////////////////////////////////////////
     // Member data
     ////////////////////////////////////////////////////////////
     std::unique_ptr<priv::WindowImpl> m_impl; //!< Platform-specific implementation of the window

src/SFML/Window/Window.cpp
@@ -63,10 +63,7 @@ Window::Window(WindowHandle handle, const ContextSettings& settings)
 
 
 ////////////////////////////////////////////////////////////
-Window::~Window()
-{
-    close();
-}
+Window::~Window() = default;
 
 
 ////////////////////////////////////////////////////////////

src/SFML/Window/WindowBase.cpp
@@ -46,16 +46,6 @@
 #include <cstdlib>
 
 
-namespace
-{
-// A nested named namespace is used here to allow unity builds of SFML.
-namespace WindowsBaseImpl
-{
-const sf::WindowBase* fullscreenWindow = nullptr;
-} // namespace WindowsBaseImpl
-} // namespace
-
-
 namespace sf
 {
 ////////////////////////////////////////////////////////////
@@ -84,10 +74,7 @@ WindowBase::WindowBase(WindowHandle handle)
 
 
 ////////////////////////////////////////////////////////////
-WindowBase::~WindowBase()
-{
-    WindowBase::close();
-}
+WindowBase::~WindowBase() = default;
 
 
 ////////////////////////////////////////////////////////////
@@ -132,10 +119,6 @@ void WindowBase::close()
 {
     // Delete the window implementation
     m_impl.reset();
-
-    // Update the fullscreen window
-    if (this == getFullscreenWindow())
-        setFullscreenWindow(nullptr);
 }
 
 
@@ -385,7 +368,7 @@ void WindowBase::create(VideoMode mode, std::uint32_t& style, State& state)
     if (state == State::Fullscreen)
     {
         // Make sure there's not already a fullscreen window (only one is allowed)
-        if (getFullscreenWindow())
+        if (m_impl->getFullscreenWindow())
         {
             err() << "Creating two fullscreen windows is not allowed, switching to windowed mode" << std::endl;
             state = State::Windowed;
@@ -403,7 +386,7 @@ void WindowBase::create(VideoMode mode, std::uint32_t& style, State& state)
             }
 
             // Update the fullscreen window
-            setFullscreenWindow(this);
+            m_impl->setFullscreenWindow();
         }
     }
 
@@ -448,20 +431,6 @@ void WindowBase::initialize()
 
     // Notify the derived class
     onCreate();
-}
-
-
-////////////////////////////////////////////////////////////
-const WindowBase* WindowBase::getFullscreenWindow()
-{
-    return WindowsBaseImpl::fullscreenWindow;
-}
-
-
-////////////////////////////////////////////////////////////
-void WindowBase::setFullscreenWindow(const WindowBase* window)
-{
-    WindowsBaseImpl::fullscreenWindow = window;
 }
 
 } // namespace sf

src/SFML/Window/WindowImpl.cpp
@@ -90,9 +90,19 @@ using WindowImplType = sf::priv::WindowImplAndroid;
 #endif
 
 
+namespace
+{
+// A nested named namespace is used here to allow unity builds of SFML.
+// Yes, this is a rather weird namespace.
+namespace WindowImplImpl
+{
+const sf::priv::WindowImpl* fullscreenWindow = nullptr;
+} // namespace WindowImplImpl
+} // namespace
+
+
 namespace sf::priv
 {
-
 ////////////////////////////////////////////////////////////
 struct WindowImpl::JoystickStatesImpl
 {
@@ -137,7 +147,11 @@ WindowImpl::WindowImpl() : m_joystickStatesImpl(std::make_unique<JoystickStatesI
 
 
 ////////////////////////////////////////////////////////////
-WindowImpl::~WindowImpl() = default;
+WindowImpl::~WindowImpl()
+{
+    if (WindowImplImpl::fullscreenWindow == this)
+        WindowImplImpl::fullscreenWindow = nullptr;
+}
 
 
 ////////////////////////////////////////////////////////////
@@ -346,6 +360,20 @@ bool WindowImpl::createVulkanSurface([[maybe_unused]] const VkInstance&         
     return VulkanImpl::createVulkanSurface(instance, getNativeHandle(), surface, allocator);
 
 #endif
+}
+
+
+////////////////////////////////////////////////////////////
+const WindowImpl* WindowImpl::getFullscreenWindow() const
+{
+    return WindowImplImpl::fullscreenWindow;
+}
+
+
+////////////////////////////////////////////////////////////
+void WindowImpl::setFullscreenWindow() const
+{
+    WindowImplImpl::fullscreenWindow = this;
 }
 
 } // namespace sf::priv

src/SFML/Window/WindowImpl.hpp
@@ -305,6 +305,20 @@ public:
     ////////////////////////////////////////////////////////////
     bool createVulkanSurface(const VkInstance& instance, VkSurfaceKHR& surface, const VkAllocationCallbacks* allocator) const;
 
+    ////////////////////////////////////////////////////////////
+    /// \brief Get the fullscreen window
+    ///
+    /// \return The fullscreen window or a null pointer if there is none
+    ///
+    ////////////////////////////////////////////////////////////
+    [[nodiscard]] const WindowImpl* getFullscreenWindow() const;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Set a window as the fullscreen window
+    ///
+    ////////////////////////////////////////////////////////////
+    void setFullscreenWindow() const;
+
 protected:
     ////////////////////////////////////////////////////////////
     /// \brief Default constructor

commit cb8fabf79191623a2b6eb51cad49584b92e703c3

Fix null pointer dereference when creating fullscreen window
Regression introduced in 4acd277

include/SFML/Window/WindowBase.hpp
@@ -492,19 +492,6 @@ private:
     friend class Window;
 
     ////////////////////////////////////////////////////////////
-    /// \brief Create (or recreate) the window
-    ///
-    /// Implementation detail for sharing underlying implementation
-    /// with sf::Window
-    ///
-    /// \param mode  Video mode to use (defines the width, height and depth of the rendering area of the window)
-    /// \param style %Window style, a bitwise OR combination of sf::Style enumerators
-    /// \param state %Window state
-    ///
-    ////////////////////////////////////////////////////////////
-    void create(VideoMode mode, std::uint32_t& style, State& state);
-
-    ////////////////////////////////////////////////////////////
     /// \brief Processes an event before it is sent to the user
     ///
     /// This function is called every time an event is received

src/SFML/Window/Window.cpp
@@ -84,9 +84,6 @@ void Window::create(VideoMode mode, const String& title, std::uint32_t style, St
 ////////////////////////////////////////////////////////////
 void Window::create(VideoMode mode, const String& title, std::uint32_t style, State state, const ContextSettings& settings)
 {
-    // Delegate to base class for creation logic
-    WindowBase::create(mode, style, state);
-
     // Recreate the window implementation
     m_impl = priv::WindowImpl::create(mode, title, style, state, settings);
 
@@ -108,9 +105,6 @@ void Window::create(WindowHandle handle)
 ////////////////////////////////////////////////////////////
 void Window::create(WindowHandle handle, const ContextSettings& settings)
 {
-    // Destroy the previous window implementation
-    close();
-
     // Recreate the window implementation
     WindowBase::create(handle);
 

src/SFML/Window/WindowBase.cpp
@@ -35,12 +35,8 @@
 #include <SFML/Window/WindowHandle.hpp>
 #include <SFML/Window/WindowImpl.hpp>
 
-#include <SFML/System/Err.hpp>
-
 #include <algorithm>
 #include <limits>
-#include <ostream>
-#include <vector>
 
 #include <cassert>
 #include <cstdlib>
@@ -88,8 +84,6 @@ WindowBase& WindowBase::operator=(WindowBase&&) noexcept = default;
 ////////////////////////////////////////////////////////////
 void WindowBase::create(VideoMode mode, const String& title, std::uint32_t style, State state)
 {
-    WindowBase::create(mode, style, state);
-
     // Recreate the window implementation
     m_impl = priv::WindowImpl::create(mode,
                                       title,
@@ -111,9 +105,6 @@ void WindowBase::create(VideoMode mode, const String& title, std::uint32_t style
 ////////////////////////////////////////////////////////////
 void WindowBase::create(WindowHandle handle)
 {
-    // Destroy the previous window implementation
-    close();
-
     // Recreate the window implementation
     m_impl = priv::WindowImpl::create(handle);
 
@@ -363,51 +354,6 @@ void WindowBase::onCreate()
 void WindowBase::onResize()
 {
     // Nothing by default
-}
-
-
-////////////////////////////////////////////////////////////
-void WindowBase::create(VideoMode mode, std::uint32_t& style, State& state)
-{
-    // Destroy the previous window implementation
-    close();
-
-    // Fullscreen style requires some tests
-    if (state == State::Fullscreen)
-    {
-        // Make sure there's not already a fullscreen window (only one is allowed)
-        if (m_impl->getFullscreenWindow())
-        {
-            err() << "Creating two fullscreen windows is not allowed, switching to windowed mode" << std::endl;
-            state = State::Windowed;
-        }
-        else
-        {
-            // Make sure that the chosen video mode is compatible
-            if (!mode.isValid())
-            {
-                err() << "The requested video mode is not available, switching to a valid mode" << std::endl;
-                assert(!VideoMode::getFullscreenModes().empty() && "No video modes available");
-                mode = VideoMode::getFullscreenModes()[0];
-                err() << "  VideoMode: { size: { " << mode.size.x << ", " << mode.size.y
-                      << " }, bitsPerPixel: " << mode.bitsPerPixel << " }" << std::endl;
-            }
-
-            // Update the fullscreen window
-            m_impl->setFullscreenWindow();
-        }
-    }
-
-// Check validity of style according to the underlying platform
-#if defined(SFML_SYSTEM_IOS) || defined(SFML_SYSTEM_ANDROID)
-    if (state == State::Fullscreen)
-        style &= ~static_cast<std::uint32_t>(Style::Titlebar);
-    else
-        style |= Style::Titlebar;
-#else
-    if ((style & Style::Close) || (style & Style::Resize))
-        style |= Style::Titlebar;
-#endif
 }
 
 

src/SFML/Window/WindowImpl.cpp
@@ -31,12 +31,14 @@
 #include <SFML/Window/SensorManager.hpp>
 #include <SFML/Window/WindowImpl.hpp>
 
+#include <SFML/System/Err.hpp>
 #include <SFML/System/Sleep.hpp>
 #include <SFML/System/Time.hpp>
 
 #include <array>
 #include <chrono>
 #include <memory>
+#include <ostream>
 
 #include <cmath>
 
@@ -118,7 +120,41 @@ std::unique_ptr<WindowImpl> WindowImpl::create(
     State                  state,
     const ContextSettings& settings)
 {
-    return std::make_unique<WindowImplType>(mode, title, style, state, settings);
+    // Fullscreen style requires some tests
+    if (state == State::Fullscreen)
+    {
+        // Make sure there's not already a fullscreen window (only one is allowed)
+        if (WindowImplImpl::fullscreenWindow != nullptr)
+        {
+            err() << "Creating two fullscreen windows is not allowed, switching to windowed mode" << std::endl;
+            state = State::Windowed;
+        }
+        // Make sure that the chosen video mode is compatible
+        else if (!mode.isValid())
+        {
+            err() << "The requested video mode is not available, switching to a valid mode" << std::endl;
+            assert(!VideoMode::getFullscreenModes().empty() && "No video modes available");
+            mode = VideoMode::getFullscreenModes()[0];
+            err() << "  VideoMode: { size: { " << mode.size.x << ", " << mode.size.y
+                  << " }, bitsPerPixel: " << mode.bitsPerPixel << " }" << std::endl;
+        }
+    }
+
+    // Check validity of style according to the underlying platform
+#if defined(SFML_SYSTEM_IOS) || defined(SFML_SYSTEM_ANDROID)
+    if (state == State::Fullscreen)
+        style &= ~static_cast<std::uint32_t>(Style::Titlebar);
+    else
+        style |= Style::Titlebar;
+#else
+    if ((style & Style::Close) || (style & Style::Resize))
+        style |= Style::Titlebar;
+#endif
+
+    auto windowImpl = std::make_unique<WindowImplType>(mode, title, style, state, settings);
+    if (state == State::Fullscreen)
+        WindowImplImpl::fullscreenWindow = windowImpl.get();
+    return windowImpl;
 }
 
 
@@ -360,20 +396,6 @@ bool WindowImpl::createVulkanSurface([[maybe_unused]] const VkInstance&         
     return VulkanImpl::createVulkanSurface(instance, getNativeHandle(), surface, allocator);
 
 #endif
-}
-
-
-////////////////////////////////////////////////////////////
-const WindowImpl* WindowImpl::getFullscreenWindow() const
-{
-    return WindowImplImpl::fullscreenWindow;
-}
-
-
-////////////////////////////////////////////////////////////
-void WindowImpl::setFullscreenWindow() const
-{
-    WindowImplImpl::fullscreenWindow = this;
 }
 
 } // namespace sf::priv

src/SFML/Window/WindowImpl.hpp
@@ -305,20 +305,6 @@ public:
     ////////////////////////////////////////////////////////////
     bool createVulkanSurface(const VkInstance& instance, VkSurfaceKHR& surface, const VkAllocationCallbacks* allocator) const;
 
-    ////////////////////////////////////////////////////////////
-    /// \brief Get the fullscreen window
-    ///
-    /// \return The fullscreen window or a null pointer if there is none
-    ///
-    ////////////////////////////////////////////////////////////
-    [[nodiscard]] const WindowImpl* getFullscreenWindow() const;
-
-    ////////////////////////////////////////////////////////////
-    /// \brief Set a window as the fullscreen window
-    ///
-    ////////////////////////////////////////////////////////////
-    void setFullscreenWindow() const;
-
 protected:
     ////////////////////////////////////////////////////////////
     /// \brief Default constructor