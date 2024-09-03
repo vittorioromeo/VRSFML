ba5d86cf857e37ef416dfeaa95ae5670b9a11398
vittorioromeo <mail@vittorioromeo.com>
2024-09-03 15:56:22 +0200

    WIP RectPacker component for texture atlases

--- src/SFML/Graphics/Image.cpp
+++ src/SFML/Graphics/Image.cpp
@@ -22,6 +22,7 @@
 #include "SFML/System/Android/ResourceStream.hpp"
 #endif
 
+#define STB_IMAGE_STATIC
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
--- src/SFML/Graphics/ImageUtils.cpp
+++ src/SFML/Graphics/ImageUtils.cpp
@@ -13,6 +13,7 @@
 
 #include "SFML/Base/Assert.hpp"
 
+#define STB_IMAGE_WRITE_STATIC
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image_write.h>
 
