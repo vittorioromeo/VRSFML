4df3dc7f1b7bd6f64dc90b76a9560387a1bcbc12
vittorioromeo <mail@vittorioromeo.com>
2024-09-12 18:10:45 +0200

    WIP

---
+++ src/SFML/Audio/MiniaudioUnity.cpp
@@ -0,0 +1,22 @@
+#include <SFML/Copyright.hpp> // LICENSE AND COPYRIGHT (C) INFORMATION
+
+// Groups multiple files using the miniaudio header in the same TU.
+
+////////////////////////////////////////////////////////////
+// Unity
+////////////////////////////////////////////////////////////
+// NOLINTBEGIN(bugprone-suspicious-include)
+#include "SFML/Audio/Unity/AudioContext.cpp"
+#include "SFML/Audio/Unity/AudioContextUtils.cpp"
+#include "SFML/Audio/Unity/AudioDeviceHandle.cpp"
+#include "SFML/Audio/Unity/CaptureDevice.cpp"
+#include "SFML/Audio/Unity/Miniaudio.cpp"
+#include "SFML/Audio/Unity/MiniaudioUtils.cpp"
+#include "SFML/Audio/Unity/PlaybackDevice.cpp"
+#include "SFML/Audio/Unity/SavedSettings.cpp"
+#include "SFML/Audio/Unity/Sound.cpp"
+#include "SFML/Audio/Unity/SoundFileReaderWav.cpp"
+#include "SFML/Audio/Unity/SoundRecorder.cpp"
+#include "SFML/Audio/Unity/SoundSource.cpp"
+#include "SFML/Audio/Unity/SoundStream.cpp"
+// NOLINTEND(bugprone-suspicious-include)
--- src/SFML/Window/Win32/Win32ImplUnity.cpp
+++ src/SFML/Window/Win32/Win32ImplUnity.cpp
@@ -1,13 +1,17 @@
 #include <SFML/Copyright.hpp> // LICENSE AND COPYRIGHT (C) INFORMATION
 
+// Groups multiple files using the windows header in the same TU.
+
 ////////////////////////////////////////////////////////////
 // Unity
 ////////////////////////////////////////////////////////////
-#include "SFML/Window/Win32/ClipboardImpl.cpp"
-#include "SFML/Window/Win32/CursorImpl.cpp"
-#include "SFML/Window/Win32/InputImpl.cpp"
-#include "SFML/Window/Win32/JoystickImpl.cpp"
-#include "SFML/Window/Win32/Utils.cpp"
-#include "SFML/Window/Win32/VideoModeImpl.cpp"
-#include "SFML/Window/Win32/VulkanImplWin32.cpp"
-#include "SFML/Window/Win32/WindowImplWin32.cpp"
+// NOLINTBEGIN(bugprone-suspicious-include)
+#include "SFML/Window/Win32/Unity/ClipboardImpl.cpp"
+#include "SFML/Window/Win32/Unity/CursorImpl.cpp"
+#include "SFML/Window/Win32/Unity/InputImpl.cpp"
+#include "SFML/Window/Win32/Unity/JoystickImpl.cpp"
+#include "SFML/Window/Win32/Unity/Utils.cpp"
+#include "SFML/Window/Win32/Unity/VideoModeImpl.cpp"
+#include "SFML/Window/Win32/Unity/VulkanImplWin32.cpp"
+#include "SFML/Window/Win32/Unity/WindowImplWin32.cpp"
+// NOLINTEND(bugprone-suspicious-include)
