630f48a0deafb186d2667c3a0d79b014ddc5fdef
Vittorio Romeo <mail@vittorioromeo.com>
2024-09-14 19:08:05 +0200

    WIP

--- examples/batching/Batching.cpp
+++ examples/batching/Batching.cpp
@@ -161,8 +161,8 @@ int main()
     // Set up UI elements
     bool         useBatch      = true;
     bool         drawSprites   = true;
-    bool         drawText      = true;
-    int          numEntities   = 100;
+    bool         drawText      = false;
+    int          numEntities   = 25000;
     unsigned int drawnVertices = 0u;
 
     //
