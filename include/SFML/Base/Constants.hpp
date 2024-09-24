b7ed7c97cc8d4a3e6022c5ac7d42c915af4bf7b7
vittorioromeo <mail@vittorioromeo.com>
2024-09-25 01:59:05 +0200

    WIP

--- include/SFML/Base/Constants.hpp
+++ include/SFML/Base/Constants.hpp
@@ -8,6 +8,9 @@ namespace sf::base
 inline constexpr float pi = 3.141592654f;
 
 ////////////////////////////////////////////////////////////
+inline constexpr float halfPi = pi / 2.f;
+
+////////////////////////////////////////////////////////////
 inline constexpr float tau = pi * 2.f;
 
 } // namespace sf::base
