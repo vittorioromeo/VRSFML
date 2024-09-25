xlin318/SFML

Fix clang-tidy-19 errors

examples/sockets/Sockets.cpp
@@ -1,13 +1,9 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
+#include "Sockets.hpp"
+
 #include <iostream>
-
-
-void runTcpServer(unsigned short port);
-void runTcpClient(unsigned short port);
-void runUdpServer(unsigned short port);
-void runUdpClient(unsigned short port);
 
 
 ////////////////////////////////////////////////////////////

examples/sockets/TCP.cpp
@@ -1,6 +1,8 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
+#include "Sockets.hpp"
+
 #include <SFML/Network.hpp>
 
 #include <iomanip>

examples/sockets/UDP.cpp
@@ -1,6 +1,8 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
+#include "Sockets.hpp"
+
 #include <SFML/Network.hpp>
 
 #include <iomanip>

examples/voip/Client.cpp
@@ -1,6 +1,8 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
+#include "VoIP.hpp"
+
 #include <SFML/Audio.hpp>
 
 #include <SFML/Network.hpp>

examples/voip/Server.cpp
@@ -1,6 +1,8 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
+#include "VoIP.hpp"
+
 #include <SFML/Audio.hpp>
 
 #include <SFML/Network.hpp>

examples/voip/VoIP.cpp
@@ -1,15 +1,9 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
+#include "VoIP.hpp"
+
 #include <iostream>
-
-
-////////////////////////////////////////////////////////////
-// Function prototypes
-// (I'm too lazy to put them into separate headers...)
-////////////////////////////////////////////////////////////
-void doClient(unsigned short port);
-void doServer(unsigned short port);
 
 
 ////////////////////////////////////////////////////////////

vittorioromeo/VRSFML

examples/sockets/Sockets.cpp
@@ -1,13 +1,10 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
+#include "TCP.hpp"
+#include "UDP.hpp"
+
 #include <iostream>
-
-
-void runTcpServer(unsigned short port);
-void runTcpClient(unsigned short port);
-void runUdpServer(unsigned short port);
-void runUdpClient(unsigned short port);
 
 
 ////////////////////////////////////////////////////////////

examples/sockets/TCP.cpp
@@ -1,6 +1,8 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
+#include "TCP.hpp"
+
 #include <SFML/Network.hpp>
 
 #include <iomanip>

examples/sockets/UDP.cpp
@@ -1,6 +1,8 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
+#include "UDP.hpp"
+
 #include <SFML/Network.hpp>
 
 #include <iomanip>

examples/voip/Client.cpp
@@ -1,6 +1,8 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
+#include "Client.hpp"
+
 #include <SFML/Audio.hpp>
 
 #include <SFML/Network.hpp>

examples/voip/Server.cpp
@@ -1,6 +1,8 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
+#include "Server.hpp"
+
 #include <SFML/Audio.hpp>
 
 #include <SFML/Network.hpp>

examples/voip/VoIP.cpp
@@ -1,15 +1,10 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
+#include "Client.hpp"
+#include "Server.hpp"
+
 #include <iostream>
-
-
-////////////////////////////////////////////////////////////
-// Function prototypes
-// (I'm too lazy to put them into separate headers...)
-////////////////////////////////////////////////////////////
-void doClient(unsigned short port);
-void doServer(unsigned short port);
 
 
 ////////////////////////////////////////////////////////////