d494fcd4bcd1bab5e395dd4f822ee8d7594048b4
vittorioromeo <vittorio.romeo@outlook.com>
2024-10-20 17:45:19 +0200

    WIP

--- run_example.sh
+++ run_example.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-basepwd=$(basename $PWD)
+basepwd=$(basename "$PWD")
 cp compile_commands.json ..
-ninja $1 && (cd ../examples/$1 && ../../$basepwd/bin/$1*)
+ninja "$1" && (
+  cd "../examples/$1" || exit
+  for suffix in ".exe" "-d.exe" "" "-d"; do
+    executable="../../$basepwd/bin/$1$suffix"
+    if [ -x "$executable" ]; then
+      "$executable"
+      exit
+    fi
+  done
+  echo "No matching executable found for $1"
+)
