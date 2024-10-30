ChrisThrasher/SFML

Improve const correctness

examples/joystick/Joystick.cpp
@@ -102,8 +102,8 @@ int main()
 
     // Set up our joystick identification sf::Text objects
     {
-        auto [it, success]   = texts.emplace("ID", JoystickObject{{font, "<Not Connected>"}, {font}});
-        auto& [label, value] = it->second;
+        const auto [it, success] = texts.emplace("ID", JoystickObject{{font, "<Not Connected>"}, {font}});
+        auto& [label, value]     = it->second;
         label.setPosition({5.f, 5.f});
         value.setPosition({80.f, 5.f});
     }
@@ -112,8 +112,8 @@ int main()
     sstr.str("");
     sstr << threshold << "  (Change with up/down arrow keys)";
     {
-        auto [it, success]   = texts.emplace("Threshold", JoystickObject{{font, "Threshold:"}, {font, sstr.str()}});
-        auto& [label, value] = it->second;
+        const auto [it, success] = texts.emplace("Threshold", JoystickObject{{font, "Threshold:"}, {font, sstr.str()}});
+        auto& [label, value]     = it->second;
         label.setPosition({5.f, 5.f + 2 * font.getLineSpacing(14)});
         value.setPosition({80.f, 5.f + 2 * font.getLineSpacing(14)});
     }
@@ -121,8 +121,9 @@ int main()
     // Set up our label-value sf::Text objects
     for (unsigned int i = 0; i < sf::Joystick::AxisCount; ++i)
     {
-        auto [it, success]   = texts.emplace(axislabels[i],
-                                           JoystickObject{{font, std::string(axislabels[i]) + ":"}, {font, "N/A"}});
+        const auto [it,
+                    success] = texts.emplace(axislabels[i],
+                                             JoystickObject{{font, std::string(axislabels[i]) + ":"}, {font, "N/A"}});
         auto& [label, value] = it->second;
         label.setPosition({5.f, 5.f + (static_cast<float>(i + 4) * font.getLineSpacing(14))});
         value.setPosition({80.f, 5.f + (static_cast<float>(i + 4) * font.getLineSpacing(14))});
@@ -132,8 +133,8 @@ int main()
     {
         sstr.str("");
         sstr << "Button " << i;
-        auto [it, success]   = texts.emplace(sstr.str(), JoystickObject{{font, sstr.str() + ":"}, {font, "N/A"}});
-        auto& [label, value] = it->second;
+        const auto [it, success] = texts.emplace(sstr.str(), JoystickObject{{font, sstr.str() + ":"}, {font, "N/A"}});
+        auto& [label, value]     = it->second;
         label.setPosition({5.f, 5.f + (static_cast<float>(sf::Joystick::AxisCount + i + 4) * font.getLineSpacing(14))});
         value.setPosition({80.f, 5.f + (static_cast<float>(sf::Joystick::AxisCount + i + 4) * font.getLineSpacing(14))});
     }

vittorioromeo/VRSFML

examples/joystick/Joystick.cpp
@@ -102,8 +102,8 @@ int main()
 
     // Set up our joystick identification sf::Text objects
     {
-        auto [it, success]   = texts.try_emplace("ID", JoystickObject{{font, "<Not Connected>"}, {font}});
-        auto& [label, value] = it->second;
+        const auto [it, success] = texts.try_emplace("ID", JoystickObject{{font, "<Not Connected>"}, {font}});
+        auto& [label, value]     = it->second;
         label.setPosition({5.f, 5.f});
         value.setPosition({80.f, 5.f});
     }
@@ -112,8 +112,8 @@ int main()
     sstr.str("");
     sstr << threshold << "  (Change with up/down arrow keys)";
     {
-        auto [it, success]   = texts.try_emplace("Threshold", JoystickObject{{font, "Threshold:"}, {font, sstr.str()}});
-        auto& [label, value] = it->second;
+        const auto [it, success] = texts.emplace("Threshold", JoystickObject{{font, "Threshold:"}, {font, sstr.str()}});
+        auto& [label, value]     = it->second;
         label.setPosition({5.f, 5.f + 2 * font.getLineSpacing(14)});
         value.setPosition({80.f, 5.f + 2 * font.getLineSpacing(14)});
     }
@@ -121,9 +121,9 @@ int main()
     // Set up our label-value sf::Text objects
     for (unsigned int i = 0; i < sf::Joystick::AxisCount; ++i)
     {
-        auto [it,
-              success] = texts.try_emplace(axislabels[i], JoystickObject{{font, axislabels[i] + ":"}, {font, "N/A"}});
-        auto& [label, value] = it->second;
+        const auto [it, success] = texts.try_emplace(axislabels[i],
+                                                     JoystickObject{{font, axislabels[i] + ":"}, {font, "N/A"}});
+        auto& [label, value]     = it->second;
         label.setPosition({5.f, 5.f + (static_cast<float>(i + 4) * font.getLineSpacing(14))});
         value.setPosition({80.f, 5.f + (static_cast<float>(i + 4) * font.getLineSpacing(14))});
     }
@@ -132,7 +132,8 @@ int main()
     {
         sstr.str("");
         sstr << "Button " << i;
-        auto [it, success]   = texts.try_emplace(sstr.str(), JoystickObject{{font, sstr.str() + ":"}, {font, "N/A"}});
+        const auto [it,
+                    success] = texts.try_emplace(sstr.str(), JoystickObject{{font, sstr.str() + ":"}, {font, "N/A"}});
         auto& [label, value] = it->second;
         label.setPosition({5.f, 5.f + (static_cast<float>(sf::Joystick::AxisCount + i + 4) * font.getLineSpacing(14))});
         value.setPosition({80.f, 5.f + (static_cast<float>(sf::Joystick::AxisCount + i + 4) * font.getLineSpacing(14))});