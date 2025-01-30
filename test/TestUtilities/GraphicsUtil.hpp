27344a13a8ffba0e4b45d9a711897495e01f8f9a
vittorioromeo <mail@vittorioromeo.com>
2025-01-31 00:34:02 +0100

    ASTRO!!!

--- test/TestUtilities/GraphicsUtil.hpp
+++ test/TestUtilities/GraphicsUtil.hpp
@@ -22,11 +22,14 @@ struct Transform;
 template <typename>
 class Rect;
 
+class COlor;
+
 std::ostream& operator<<(std::ostream& os, const BlendMode& blendMode);
 std::ostream& operator<<(std::ostream& os, const StencilComparison& comparison);
 std::ostream& operator<<(std::ostream& os, const StencilUpdateOperation& updateOperation);
 std::ostream& operator<<(std::ostream& os, const StencilMode& stencilMode);
 std::ostream& operator<<(std::ostream& os, Color color);
+std::ostream& operator<<(std::ostream& os, Approx<Color> color);
 std::ostream& operator<<(std::ostream& os, const Transform& transform);
 
 template <typename T>
@@ -34,6 +37,7 @@ std::ostream& operator<<(std::ostream& os, const Rect<T>& rect);
 } // namespace sf
 
 bool operator==(const sf::Transform& lhs, const Approx<sf::Transform>& rhs);
+bool operator==(const sf::Color& lhs, const Approx<sf::Color>& rhs);
 
 template <typename T>
 bool operator==(const sf::Rect<T>& lhs, const Approx<sf::Rect<T>>& rhs)
