6c913b353a129000221e9d77b0737e7944ee5107
vittorioromeo <mail@vittorioromeo.com>
2025-02-04 02:06:35 +0100

    WIP

---
+++ examples/bubble_idle/Bubble.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "Aliases.hpp"
+
+#include "SFML/Graphics/Sprite.hpp"
+
+#include "SFML/System/Vector2.hpp"
+
+
+////////////////////////////////////////////////////////////
+enum class [[nodiscard]] BubbleType : U8
+{
+    Normal = 0u,
+    Star   = 1u,
+    Bomb   = 2u
+};
+
+////////////////////////////////////////////////////////////
+struct [[nodiscard]] Bubble
+{
+    sf::Vector2f position;
+    sf::Vector2f velocity;
+
+    float scale;
+    float rotation;
+
+    BubbleType type;
+
+    ////////////////////////////////////////////////////////////
+    [[gnu::always_inline]] inline void update(const float deltaTime)
+    {
+        position += velocity * deltaTime;
+    }
+
+    ////////////////////////////////////////////////////////////
+    [[gnu::always_inline]] inline void applyToSprite(sf::Sprite& sprite) const
+    {
+        sprite.position = position;
+        sprite.scale    = {scale, scale};
+        sprite.rotation = sf::radians(rotation);
+    }
+
+    ////////////////////////////////////////////////////////////
+    [[nodiscard, gnu::always_inline]] inline float getRadius() const
+    {
+        return 256.f * scale;
+    }
+};
---
+++ examples/bubble_idle/Cat.hpp
@@ -0,0 +1,124 @@
+#pragma once
+
+#include "Aliases.hpp"
+#include "Countdown.hpp"
+#include "LoopingTimer.hpp"
+#include "TextShakeEffect.hpp"
+
+#include "SFML/System/Vector2.hpp"
+
+#include "SFML/Base/IntTypes.hpp"
+#include "SFML/Base/Optional.hpp"
+#include "SFML/Base/SizeT.hpp"
+
+
+////////////////////////////////////////////////////////////
+enum class [[nodiscard]] CatType : sf::base::U8
+{
+    Normal = 0u,
+    Uni    = 1u,
+    Devil  = 2u,
+    Witch  = 3u,
+    Astro  = 4u,
+
+    Count
+};
+
+////////////////////////////////////////////////////////////
+inline constexpr auto nCatTypes = static_cast<sf::base::SizeT>(CatType::Count);
+
+////////////////////////////////////////////////////////////
+struct [[nodiscard]] Cat
+{
+    sf::Vector2f position;
+    sf::Vector2f rangeOffset;
+
+    LoopingTimer wobbleTimer;
+    LoopingTimer cooldownTimer;
+
+    sf::Vector2f pawPosition;
+    sf::Angle    pawRotation;
+
+    float mainOpacity = 255.f;
+    float pawOpacity  = 255.f;
+
+    Countdown inspiredCountdown;
+
+    SizeT nameIdx;
+
+    TextShakeEffect textStatusShakeEffect;
+
+    sf::base::U32 hits = 0u;
+
+    CatType type;
+
+    ////////////////////////////////////////////////////////////
+    struct [[nodiscard]] AstroState
+    {
+        float startX;
+
+        float velocityX     = 0.f;
+        float particleTimer = 0.f;
+
+        bool wrapped = false;
+    };
+
+    sf::base::Optional<AstroState> astroState;
+
+    ////////////////////////////////////////////////////////////
+    [[gnu::always_inline]] inline void update(const float deltaTime)
+    {
+        textStatusShakeEffect.update(deltaTime);
+        (void)wobbleTimer.updateAndLoop(deltaTime * 0.002f, sf::base::tau);
+    }
+
+    ////////////////////////////////////////////////////////////
+    [[nodiscard, gnu::always_inline]] inline sf::Vector2f getDrawPosition() const
+    {
+        return position + sf::Vector2f{0.f, std::sin(wobbleTimer.value * 2.f) * 7.5f};
+    }
+
+    ////////////////////////////////////////////////////////////
+    [[nodiscard, gnu::always_inline]] inline bool updateCooldown(const float maxCooldown, const float deltaTime)
+    {
+        const float cooldownSpeed = inspiredCountdown.updateAndIsActive(deltaTime) ? 2.f : 1.f;
+        return cooldownTimer.updateAndStop(deltaTime * cooldownSpeed, maxCooldown);
+    }
+
+    ////////////////////////////////////////////////////////////
+    [[nodiscard, gnu::always_inline]] inline float getRadius() const noexcept
+    {
+        return 64.f;
+    }
+
+    ////////////////////////////////////////////////////////////
+    [[nodiscard, gnu::always_inline]] inline bool isCloseToStartX() const noexcept
+    {
+        SFML_BASE_ASSERT(type == CatType::Astro);
+        SFML_BASE_ASSERT(astroState.hasValue());
+
+        return astroState->wrapped && sf::base::fabs(position.x - astroState->startX) < 400.f;
+    }
+
+    ////////////////////////////////////////////////////////////
+    [[nodiscard, gnu::always_inline]] inline bool isAstroAndInFlight() const noexcept
+    {
+        return type == CatType::Astro && astroState.hasValue();
+    }
+};
+
+////////////////////////////////////////////////////////////
+[[nodiscard]] inline Cat makeCat(const CatType catType, const sf::Vector2f& position, const sf::Vector2f rangeOffset, const SizeT nameIdx)
+{
+    return Cat{.position              = position,
+               .rangeOffset           = rangeOffset,
+               .wobbleTimer           = {},
+               .cooldownTimer         = {},
+               .pawPosition           = position,
+               .pawRotation           = sf::radians(0.f),
+               .inspiredCountdown     = {},
+               .nameIdx               = nameIdx,
+               .textStatusShakeEffect = {},
+               .type                  = catType,
+               .astroState            = {}};
+}
