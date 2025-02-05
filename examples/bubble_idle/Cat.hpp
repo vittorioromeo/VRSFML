cf6ff7a613466778eaaafcc6e49365a36cb37b3c
vittorioromeo <mail@vittorioromeo.com>
2025-02-05 02:58:28 +0100

    WIP shrines

--- examples/bubble_idle/Cat.hpp
+++ examples/bubble_idle/Cat.hpp
@@ -122,3 +122,52 @@ struct [[nodiscard]] Cat
                .type                  = catType,
                .astroState            = {}};
 }
+
+
+////////////////////////////////////////////////////////////
+struct [[nodiscard]] Shrine
+{
+    sf::Vector2f position;
+
+    LoopingTimer wobbleTimer{};
+
+    float mainOpacity = 255.f;
+
+    bool  dying     = false;
+    float deathTime = 5000.f;
+
+    TextShakeEffect textStatusShakeEffect;
+
+    sf::base::U64 requiredReward;
+
+    sf::base::U64 collectedReward = 0u;
+
+    ////////////////////////////////////////////////////////////
+    [[nodiscard, gnu::always_inline]] inline float getDeathProgress() const
+    {
+        return 1.f - (deathTime / 5000.f);
+    }
+
+    ////////////////////////////////////////////////////////////
+    [[gnu::always_inline]] inline void update(const float deltaTime)
+    {
+        if (dying)
+            deathTime -= deltaTime;
+
+        textStatusShakeEffect.update(deltaTime);
+        (void)wobbleTimer.updateAndLoop(deltaTime * 0.002f + getDeathProgress() * 0.2f, sf::base::tau);
+    }
+
+    ////////////////////////////////////////////////////////////
+    [[nodiscard, gnu::always_inline]] inline sf::Vector2f getDrawPosition() const
+    {
+        return position + sf::Vector2f{std::cos(wobbleTimer.value) * (7.5f + getDeathProgress() * 128.f),
+                                       std::sin(wobbleTimer.value) * (14.f + getDeathProgress() * 128.f)};
+    }
+
+    ////////////////////////////////////////////////////////////
+    [[nodiscard, gnu::always_inline]] inline float getRange() const
+    {
+        return 256.f * (1.f - getDeathProgress());
+    }
+};
