feb27f2acbc99e79c375e1255efdc7284a34c6b0
vittorioromeo <mail@vittorioromeo.com>
2025-03-04 02:26:24 +0100

    WIP

--- examples/bubble_idle/Version.hpp
+++ examples/bubble_idle/Version.hpp
@@ -14,7 +14,7 @@
 #define BUBBLEBYTE_VERSION_MINOR 0
 
 ////////////////////////////////////////////////////////////
-#define BUBBLEBYTE_VERSION_PATCH 12
+#define BUBBLEBYTE_VERSION_PATCH 13
 
 ////////////////////////////////////////////////////////////
 #define BUBBLEBYTE_VERSION_STR                                                   \
