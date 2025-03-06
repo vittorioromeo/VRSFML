b091b1296c42d2f936d6cb2932d2bf2e6e06d9a6
vittorioromeo <mail@vittorioromeo.com>
2025-03-06 23:32:18 +0100

    WIP

--- test/Graphics/Texture.test.cpp
+++ test/Graphics/Texture.test.cpp
@@ -41,7 +41,7 @@ TEST_CASE("[Graphics] sf::Texture" * doctest::skip(skipDisplayTests))
             CHECK(texture.getSize() == sf::Vector2u{64, 64});
             CHECK(!texture.isSmooth());
             CHECK(!texture.isSrgb());
-            CHECK(!texture.isRepeated());
+            CHECK(texture.getWrapMode() == sf::TextureWrapMode::Clamp);
             CHECK(texture.getNativeHandle() != 0);
         }
 
@@ -53,7 +53,7 @@ TEST_CASE("[Graphics] sf::Texture" * doctest::skip(skipDisplayTests))
             CHECK(texture.getSize() == sf::Vector2u{64, 64});
             CHECK(!texture.isSmooth());
             CHECK(!texture.isSrgb());
-            CHECK(!texture.isRepeated());
+            CHECK(texture.getWrapMode() == sf::TextureWrapMode::Clamp);
             CHECK(texture.getNativeHandle() != 0);
         }
     }
@@ -87,7 +87,7 @@ TEST_CASE("[Graphics] sf::Texture" * doctest::skip(skipDisplayTests))
         CHECK(texture.getSize() == sf::Vector2u{1001, 304});
         CHECK(!texture.isSmooth());
         CHECK(!texture.isSrgb());
-        CHECK(!texture.isRepeated());
+        CHECK(texture.getWrapMode() == sf::TextureWrapMode::Clamp);
         CHECK(texture.getNativeHandle() != 0);
     }
 
@@ -98,7 +98,7 @@ TEST_CASE("[Graphics] sf::Texture" * doctest::skip(skipDisplayTests))
         CHECK(texture.getSize() == sf::Vector2u{1001, 304});
         CHECK(!texture.isSmooth());
         CHECK(!texture.isSrgb());
-        CHECK(!texture.isRepeated());
+        CHECK(texture.getWrapMode() == sf::TextureWrapMode::Clamp);
         CHECK(texture.getNativeHandle() != 0);
     }
 
@@ -109,7 +109,7 @@ TEST_CASE("[Graphics] sf::Texture" * doctest::skip(skipDisplayTests))
         CHECK(texture.getSize() == sf::Vector2u{1001, 304});
         CHECK(!texture.isSmooth());
         CHECK(!texture.isSrgb());
-        CHECK(!texture.isRepeated());
+        CHECK(texture.getWrapMode() == sf::TextureWrapMode::Clamp);
         CHECK(texture.getNativeHandle() != 0);
     }
 
@@ -248,11 +248,11 @@ TEST_CASE("[Graphics] sf::Texture" * doctest::skip(skipDisplayTests))
     SECTION("Set/get repeated")
     {
         auto texture = sf::Texture::create({64, 64}).value();
-        CHECK(!texture.isRepeated());
-        texture.setRepeated(true);
-        CHECK(texture.isRepeated());
-        texture.setRepeated(false);
-        CHECK(!texture.isRepeated());
+        CHECK(texture.getWrapMode() == sf::TextureWrapMode::Clamp);
+        texture.setWrapMode(sf::TextureWrapMode::Repeat);
+        CHECK(texture.getWrapMode() == sf::TextureWrapMode::Repeat);
+        texture.setWrapMode(sf::TextureWrapMode::Clamp);
+        CHECK(texture.getWrapMode() == sf::TextureWrapMode::Clamp);
     }
 
     SECTION("generateMipmap()")
@@ -269,20 +269,20 @@ TEST_CASE("[Graphics] sf::Texture" * doctest::skip(skipDisplayTests))
         auto texture1 = sf::Texture::create(sf::Vector2u{1, 1}, {.sRgb = true}).value();
         texture1.update(blue);
         texture1.setSmooth(false);
-        texture1.setRepeated(true);
+        texture1.setWrapMode(sf::TextureWrapMode::Repeat);
 
         auto texture2 = sf::Texture::create(sf::Vector2u{2, 1}, {.sRgb = false}).value();
         texture2.update(green);
         texture2.setSmooth(true);
-        texture2.setRepeated(false);
+        texture2.setWrapMode(sf::TextureWrapMode::Clamp);
 
         sf::swap(texture1, texture2);
         CHECK_FALSE(texture1.isSrgb());
         CHECK(texture1.isSmooth());
-        CHECK_FALSE(texture1.isRepeated());
+        CHECK_FALSE(texture1.getWrapMode() == sf::TextureWrapMode::Repeat);
         // Cannot check texture2.isSrgb() because Srgb is sometimes disabled when using OpenGL ES
         CHECK_FALSE(texture2.isSmooth());
-        CHECK(texture2.isRepeated());
+        CHECK(texture2.getWrapMode() == sf::TextureWrapMode::Repeat);
 
         const auto image1 = texture1.copyToImage();
         const auto image2 = texture2.copyToImage();
