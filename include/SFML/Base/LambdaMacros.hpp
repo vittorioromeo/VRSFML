48998d75f49a149a0d3dc2b96c013412c8eae50b
vittorioromeo <vittorio.romeo@outlook.com>
2025-03-14 18:53:22 +0100

    wip

---
+++ include/SFML/Base/LambdaMacros.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <SFML/Copyright.hpp> // LICENSE AND COPYRIGHT (C) INFORMATION
+
+
+////////////////////////////////////////////////////////////
+#define SFML_BASE_LAMBDA_ALWAYS_INLINE_FLATTEN __attribute__((always_inline, flatten))
+
+////////////////////////////////////////////////////////////
+#define SFML_BASE_LAMBDA_ALWAYS_INLINE __attribute__((always_inline))
