55f07787625c50ec00b060e51c4222af65ff62c7
vittorioromeo <vittorio.romeo@outlook.com>
2025-03-19 17:52:39 +0100

    wip

--- src/SFML/GLUtils/EGL/EGLContext.cpp
+++ src/SFML/GLUtils/EGL/EGLContext.cpp
@@ -4,9 +4,10 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
-#include "SFML/Window/VideoMode.hpp"
-#include "SFML/Window/VideoModeUtils.hpp"
-#include "SFML/Window/WindowContext.hpp"
+#include "SFML/SFML/Window/EGL/ext.hpp"
+#include "SFMLSFML/Window/EGL/Utils.hpp"
+
+#include "SFML/Window/EGL/Mode.hpp"
 #include "SFML/Window/WindowImpl.hpp"
 
 #include "SFML/GLUtils/EGL/EGLCheck.hpp"
@@ -26,53 +27,9 @@
 #endif
 
 #if defined(SFML_SYSTEM_LINUX) && !defined(SFML_USE_DRM)
+    #include "SFML/Window/Unix/Utils.hpp"
 
-    #include "SFML/Base/UniquePtr.hpp"
     #include <X11/Xlib.h>
-
-namespace sf::priv
-{
-template <typename T>
-struct RemoveArrayExtentsImpl
-{
-    using type = T;
-};
-
-template <typename T>
-struct RemoveArrayExtentsImpl<T[]>
-{
-    using type = T;
-};
-
-template <typename T>
-using RemoveArrayExtents = typename RemoveArrayExtentsImpl<T>::type;
-
-////////////////////////////////////////////////////////////
-/// \brief Class template for freeing X11 pointers
-///
-/// Specialized elsewhere for types that are freed through
-/// other means than XFree(). XFree() is the most common use
-/// case though so it is the default.
-///
-////////////////////////////////////////////////////////////
-template <typename T>
-struct XDeleter
-{
-    void operator()(T* data) const
-    {
-        if (data != nullptr)
-            XFree(data); // NOLINT(bugprone-multi-level-implicit-pointer-conversion)
-    }
-};
-
-////////////////////////////////////////////////////////////
-/// \brief Class template for wrapping owning raw pointers from X11
-///
-////////////////////////////////////////////////////////////
-template <typename T>
-using X11Ptr = base::UniquePtr<RemoveArrayExtents<T>, XDeleter<RemoveArrayExtents<T>>>;
-} // namespace sf::priv
-
 #endif
 
 
