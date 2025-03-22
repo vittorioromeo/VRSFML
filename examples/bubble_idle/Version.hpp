3bf3409b439a591c71a94169d3b154a947a83566
vittorioromeo <mail@vittorioromeo.com>
2025-03-22 18:14:54 +0100

    v1.5.0

--- examples/bubble_idle/Version.hpp
+++ examples/bubble_idle/Version.hpp
@@ -11,10 +11,10 @@
 #define BUBBLEBYTE_VERSION_MAJOR 1
 
 ////////////////////////////////////////////////////////////
-#define BUBBLEBYTE_VERSION_MINOR 4
+#define BUBBLEBYTE_VERSION_MINOR 5
 
 ////////////////////////////////////////////////////////////
-#define BUBBLEBYTE_VERSION_PATCH 1
+#define BUBBLEBYTE_VERSION_PATCH 0
 
 ////////////////////////////////////////////////////////////
 #define BUBBLEBYTE_VERSION_STR                                                   \
@@ -27,7 +27,18 @@ struct [[nodiscard]] Version
     unsigned int major;
     unsigned int minor;
     unsigned int patch;
+
+    constexpr bool operator==(const Version& other) const = default;
 };
 
 ////////////////////////////////////////////////////////////
 inline constexpr Version currentVersion{BUBBLEBYTE_VERSION_MAJOR, BUBBLEBYTE_VERSION_MINOR, BUBBLEBYTE_VERSION_PATCH};
+
+
+// TODO P0: changelog
+// - Decrease cost of selective starpaw from 8PP to 4PP
+// - Add option to tweak the amount of time required to hold the mouse button to trigger cat dragging
+// - Sligthly increased amount of time required to start dragging cats
+// - Changed PP reward formula from 2^level to 2^(level+1)
+// - Older saves will get compensated for the new PP reward formula
+// - Autocast
