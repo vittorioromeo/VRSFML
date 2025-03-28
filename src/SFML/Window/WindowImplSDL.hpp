7dcb65ef5a62c2fdecdc697ba1728cbb065b79f7
vittorioromeo <vittorio.romeo@outlook.com>
2025-03-28 01:53:36 +0100

    WIP

---
+++ src/SFML/Window/WindowImplSDL.hpp
@@ -0,0 +1,200 @@
+#pragma once
+#include <SFML/Copyright.hpp> // LICENSE AND COPYRIGHT (C) INFORMATION
+
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include "SFML/Window/Keyboard.hpp"
+#include "SFML/Window/WindowHandle.hpp"
+#include "SFML/Window/WindowImpl.hpp"
+
+#include "SFML/System/Vector2.hpp"
+
+#include "SFML/Base/IntTypes.hpp"
+
+#include <SDL3/SDL_video.h>
+
+
+namespace sf
+{
+class String;
+struct WindowSettings;
+
+namespace priv
+{
+////////////////////////////////////////////////////////////
+/// \brief Windows implementation of WindowImpl
+///
+////////////////////////////////////////////////////////////
+class [[nodiscard]] WindowImplSDL : public WindowImpl
+{
+public:
+    ////////////////////////////////////////////////////////////
+    /// \brief Construct the window implementation from an existing control
+    ///
+    /// \param handle Platform-specific handle of the control
+    ///
+    ////////////////////////////////////////////////////////////
+    [[nodiscard]] explicit WindowImplSDL(WindowHandle handle);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Create the window implementation
+    ///
+    /// \param windowSettings Window settings
+    ///
+    ////////////////////////////////////////////////////////////
+    [[nodiscard]] explicit WindowImplSDL(const WindowSettings& windowSettings);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Destructor
+    ///
+    ////////////////////////////////////////////////////////////
+    ~WindowImplSDL() override;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Get the OS-specific handle of the window
+    ///
+    /// \return Handle of the window
+    ///
+    ////////////////////////////////////////////////////////////
+    [[nodiscard]] WindowHandle getNativeHandle() const override;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Get the position of the window
+    ///
+    /// \return Position of the window, in pixels
+    ///
+    ////////////////////////////////////////////////////////////
+    [[nodiscard]] Vector2i getPosition() const override;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Change the position of the window on screen
+    ///
+    /// \param position New position of the window, in pixels
+    ///
+    ////////////////////////////////////////////////////////////
+    void setPosition(Vector2i position) override;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Get the client size of the window
+    ///
+    /// \return Size of the window, in pixels
+    ///
+    ////////////////////////////////////////////////////////////
+    [[nodiscard]] Vector2u getSize() const override;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Change the size of the rendering region of the window
+    ///
+    /// \param size New size, in pixels
+    ///
+    ////////////////////////////////////////////////////////////
+    void setSize(Vector2u size) override;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Change the title of the window
+    ///
+    /// \param title New title
+    ///
+    ////////////////////////////////////////////////////////////
+    void setTitle(const String& title) override;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Change the window's icon
+    ///
+    /// \param size   Icon's width and height, in pixels
+    /// \param pixels Pointer to the pixels in memory, format must be RGBA 32 bits
+    ///
+    ////////////////////////////////////////////////////////////
+    void setIcon(Vector2u size, const base::U8* pixels) override;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Show or hide the window
+    ///
+    /// \param visible `true` to show, `false` to hide
+    ///
+    ////////////////////////////////////////////////////////////
+    void setVisible(bool visible) override;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Show or hide the mouse cursor
+    ///
+    /// \param visible `true` to show, `false` to hide
+    ///
+    ////////////////////////////////////////////////////////////
+    void setMouseCursorVisible(bool visible) override;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Grab or release the mouse cursor
+    ///
+    /// \param grabbed `true` to enable, `false` to disable
+    ///
+    ////////////////////////////////////////////////////////////
+    void setMouseCursorGrabbed(bool grabbed) override;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Set the displayed cursor to a native system cursor
+    ///
+    /// \param cursor Native system cursor type to display
+    ///
+    ////////////////////////////////////////////////////////////
+    void setMouseCursor(const CursorImpl& cursor) override;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Enable or disable automatic key-repeat
+    ///
+    /// \param enabled `true` to enable, `false` to disable
+    ///
+    ////////////////////////////////////////////////////////////
+    void setKeyRepeatEnabled(bool enabled) override;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Request the current window to be made the active
+    ///        foreground window
+    ///
+    ////////////////////////////////////////////////////////////
+    void requestFocus() override;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Check whether the window has the input focus
+    ///
+    /// \return `true` if window has focus, `false` otherwise
+    ///
+    ////////////////////////////////////////////////////////////
+    [[nodiscard]] bool hasFocus() const override;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Return a scaling factor for DPI-aware scaling
+    ///
+    /// \return `1.f` for default DPI (96) or the window is not
+    ///         DPI-aware, otherwise the scaling factor
+    ///
+    ////////////////////////////////////////////////////////////
+    [[nodiscard]] float getDPIAwareScalingFactor() const override;
+
+protected:
+    ////////////////////////////////////////////////////////////
+    /// \brief Process incoming events from the operating system
+    ///
+    ////////////////////////////////////////////////////////////
+    void processEvents() override;
+
+private:
+    ////////////////////////////////////////////////////////////
+    /// \brief Construct the window from an SDL window pointer
+    ///
+    /// \param context   Creation context for error messages
+    /// \param sdlWindow SDL window handle
+    ///
+    ////////////////////////////////////////////////////////////
+    [[nodiscard]] explicit WindowImplSDL(const char* context, SDL_Window* sdlWindow);
+
+    ////////////////////////////////////////////////////////////
+    // Member data
+    ////////////////////////////////////////////////////////////
+    SDL_Window* m_sdlWindow; //!< SDL window handle
+};
+
+} // namespace priv
+} // namespace sf
