ef5a6e12964373aea79cd57b6f19095364bd73b6
vittorioromeo <vittorio.romeo@outlook.com>
2025-03-29 18:03:06 +0100

    WIP

---
+++ src/SFML/Window/SDLGlContext.hpp
@@ -0,0 +1,50 @@
+#pragma once
+#include "SFML/GLUtils/GlContext.hpp"
+
+#include <SFML/Copyright.hpp>
+
+////////////////////////////////////////////////////////////
+struct SDL_Window;
+struct SDL_GLContextState;
+using SDL_GLContext = SDL_GLContextState*;
+
+namespace sf::priv
+{
+class WindowImpl;
+
+////////////////////////////////////////////////////////////
+class SDLGlContext : public GlContext
+{
+public:
+    ////////////////////////////////////////////////////////////
+    [[nodiscard]] explicit SDLGlContext(unsigned int id, SDLGlContext* shared);
+
+    ////////////////////////////////////////////////////////////
+    [[nodiscard]] explicit SDLGlContext(unsigned int           id,
+                                        SDLGlContext*          shared,
+                                        const ContextSettings& settings,
+                                        const WindowImpl&      owner,
+                                        unsigned int           bitsPerPixel);
+
+    ////////////////////////////////////////////////////////////
+    ~SDLGlContext() override;
+
+    ////////////////////////////////////////////////////////////
+    GlFunctionPointer getFunction(const char* name) const;
+
+    ////////////////////////////////////////////////////////////
+    bool makeCurrent(bool current) override;
+
+    ////////////////////////////////////////////////////////////
+    void display() override;
+
+    ////////////////////////////////////////////////////////////
+    void setVerticalSyncEnabled(bool enabled) override;
+
+private:
+    ////////////////////////////////////////////////////////////
+    SDL_Window*   m_window;     // SDL window associated with the context
+    SDL_GLContext m_context;    // SDL OpenGL context handle
+    bool          m_ownsWindow; // Whether the context owns the window (for offscreen contexts)
+};
+} // namespace sf::priv
