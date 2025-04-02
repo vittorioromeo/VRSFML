b84d33d398821ef63cba4f223b57ded23ab7d8e7
vittorioromeo <mail@vittorioromeo.com>
2025-04-02 15:55:52 +0200

    1.7.0

--- examples/bubble_idle/Version.hpp
+++ examples/bubble_idle/Version.hpp
@@ -35,24 +35,23 @@ struct [[nodiscard]] Version
 inline constexpr Version currentVersion{BUBBLEBYTE_VERSION_MAJOR, BUBBLEBYTE_VERSION_MINOR, BUBBLEBYTE_VERSION_PATCH};
 
 
-// TODO P0: CHANGELOG (already created, must make public)
-// - Added left control as another valid key for AOE selection
-// - Performing an AOE selection near the edge of the screen will now scroll
-// - Wisdom point counter in "Magic" menu now uses digit separators
-// - It is now possible to update spells with wisdom points while the wizard is busy (e.g. absorbing)
-// - The multipop circle range indicator is now hidden while dragging cats
-// - Added a SFX volume slider
-// - Fixed a crash related to the Wizard's Mewltiplier Aura spell
-// - Changed #astrocats achievements fromm [1,5,10,20,30,40] to [1,5,10,20,25,30]
-// - Improved statistics panel to avoid text going out of bounds
-// - Added checkbox in in-game achivements menu to show/hide completed achievements
-
-// TODO P0: ADD TO PATCH NOTES
-// - In-game achievements are now always synced with Steam
-// - Made it possible to bulk purchase PPs in end game (show screenshot)
-// - Long button labels are now scaled to avoid the text going out of bounds
-// - Maxed-out button tooltips will now show "MAXED OUT" instead of calculating one more step
-// - Achievement progress for most achievements is now displayed in-game
-// - Sligthly increased menu scrollbar thickness
-// - Added ability to hide maxed-out purchasables and category separators in shop ("Interface" tab)
-// - Added "UNDO" button to undo PP purchases, can be used multiple times within 10s
+// x - Added left control as another valid key for AOE selection
+// x - Performing an AOE selection near the edge of the screen will now scroll
+// x - Wisdom point counter in "Magic" menu now uses digit separators
+// x - It is now possible to update spells with wisdom points while the wizard is busy (e.g. absorbing)
+// x - The multipop circle range indicator is now hidden while dragging cats
+// x - Added a SFX volume slider
+// x - Fixed a crash related to the Wizard's Mewltiplier Aura spell
+// x - Improved statistics panel to avoid text going out of bounds
+// x - Made it possible to bulk purchase PPs in end game (show screenshot)
+// x - Long button labels are now scaled to avoid the text going out of bounds
+// x - Maxed-out button tooltips will now show "MAXED OUT" instead of calculating one more step
+// x - Sligthly increased menu scrollbar thickness
+// x - Added ability to hide maxed-out purchasables and category separators in shop ("Interface" tab)
+// x - Fixed "Remember to buy combo upgrade tip" showing up after prestige
+// x - Slightly increased default cursor scale
+// x - In-game achievements are now always synced with Steam
+// x - Achievement progress for most achievements is now displayed in-game
+// x - Changed #astrocats achievements from [1,5,10,20,30,40] to [1,5,10,20,25,30]
+// x - Added checkbox in in-game achivements menu to show/hide completed achievements
+// x - Added "UNDO" button to undo PP purchases, can be used multiple times within 10s
