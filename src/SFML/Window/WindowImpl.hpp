3bf885673e0a11a3c76b18ef8ddc3915c61047ed
Vittorio Romeo <mail@vittorioromeo.com>
2025-04-18 18:41:44 +0200

    WIP

--- src/SFML/Window/WindowImpl.hpp
+++ src/SFML/Window/WindowImpl.hpp
@@ -332,9 +332,10 @@ private:
     /// \param context   Creation context for error messages
     /// \param sdlWindow SDL window handle
     /// \param isExternal `true` if the window was created externally
+    /// \param lastSize Last known size of the window
     ///
     ////////////////////////////////////////////////////////////
-    [[nodiscard]] explicit WindowImpl(const char* context, void* sdlWindow, bool isExternal);
+    [[nodiscard]] explicit WindowImpl(const char* context, void* sdlWindow, bool isExternal, Vector2u lastSize);
 
     ////////////////////////////////////////////////////////////
     /// \return First event of the queue if available, `base::nullOpt` otherwise
