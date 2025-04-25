cc7c9a4816ea43f983b19190508a02be93fcbe67
vittorioromeo <vittorio.romeo@outlook.com>
2025-04-25 17:02:57 +0200

    WIPG

--- include/SFML/GLUtils/GLVAOGroup.hpp
+++ include/SFML/GLUtils/GLVAOGroup.hpp
@@ -5,8 +5,13 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
+#include "SFML/Graphics/GraphicsContext.hpp"
+
 #include "SFML/GLUtils/GLBufferObject.hpp"
-#include "SFML/GLUtils/GLVertexArrayObject.hpp"
+
+#include "SFML/System/Err.hpp"
+
+#include "SFML/Base/AnkerlUnorderedDense.hpp"
 
 
 namespace sf
@@ -18,28 +23,82 @@ namespace sf
 struct GLVAOGroup
 {
     ////////////////////////////////////////////////////////////
-    GLVertexArrayObject   vao; //!< Vertex array object
-    GLVertexBufferObject  vbo; //!< Associated vertex buffer object
-    GLElementBufferObject ebo; //!< Associated element index buffer object
+    mutable ankerl::unordered_dense::map<unsigned int, unsigned int> perContextVAOIds; //!< Vertex array object (unshared, per context)
+
+    ////////////////////////////////////////////////////////////
+    GLVertexBufferObject  vbo; //!< Vertex buffer object (shared context)
+    GLElementBufferObject ebo; //!< Element index buffer object (shared context)
+
+    ////////////////////////////////////////////////////////////
+    explicit GLVAOGroup() = default;
+
+    ////////////////////////////////////////////////////////////
+    ~GLVAOGroup()
+    {
+        SFML_BASE_ASSERT(GraphicsContext::hasActiveThreadLocalGlContext());
+        const unsigned int glContextId = GraphicsContext::getActiveThreadLocalGlContextId();
+
+        auto* it = perContextVAOIds.find(glContextId);
+
+        if (it != perContextVAOIds.end())
+            GraphicsContext::unregisterUnsharedVAO(glContextId, it->second);
+    }
+
+    ////////////////////////////////////////////////////////////
+    GLVAOGroup(const GLVAOGroup&)            = delete;
+    GLVAOGroup& operator=(const GLVAOGroup&) = delete;
+
+    ////////////////////////////////////////////////////////////
+    GLVAOGroup(GLVAOGroup&&) noexcept            = default;
+    GLVAOGroup& operator=(GLVAOGroup&&) noexcept = default;
 
     ////////////////////////////////////////////////////////////
     [[gnu::always_inline, gnu::flatten]] void bind() const
     {
-        vao.bind();
+        SFML_BASE_ASSERT(GraphicsContext::hasActiveThreadLocalGlContext());
+        const unsigned int glContextId = GraphicsContext::getActiveThreadLocalGlContextId();
+
+        const auto* it = perContextVAOIds.find(glContextId);
+
+        if (it != perContextVAOIds.end())
+        {
+            glCheck(glBindVertexArray(it->second));
+        }
+        else
+        {
+            unsigned int id{};
+            glCheck(glGenVertexArrays(1, &id));
+
+            if (id == 0u)
+            {
+                priv::err() << "Failed to create a vertex array object";
+                return;
+            }
+
+            auto res = perContextVAOIds.try_emplace(glContextId, id);
+
+            const unsigned int vaoId = res.first->second;
+
+            glCheck(glBindVertexArray(vaoId));
+            GraphicsContext::registerUnsharedVAO(glContextId, vaoId);
+        }
+
         vbo.bind();
         ebo.bind();
     }
 
     ////////////////////////////////////////////////////////////
-    [[nodiscard, gnu::always_inline, gnu::flatten]] static unsigned int getBoundId()
+    [[nodiscard, gnu::always_inline, gnu::flatten]] unsigned int getId() const
     {
-        return GLVertexArrayObject::getBoundId();
-    }
+        SFML_BASE_ASSERT(GraphicsContext::hasActiveThreadLocalGlContext());
+        const unsigned int glContextId = GraphicsContext::getActiveThreadLocalGlContextId();
 
-    ////////////////////////////////////////////////////////////
-    [[nodiscard, gnu::always_inline, gnu::flatten, gnu::pure]] unsigned int getId() const
-    {
-        return vao.getId();
+        const auto* it = perContextVAOIds.find(glContextId);
+
+        if (it != perContextVAOIds.end())
+            return it->second;
+
+        return 0u;
     }
 };
 
