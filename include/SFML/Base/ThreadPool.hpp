62ce314f8dcc8c517fca6bbea162f0fdfd5e7fa4
vittorioromeo <vittorio.romeo@outlook.com>
2025-04-27 04:15:08 +0200

    WIP

--- include/SFML/Base/ThreadPool.hpp
+++ include/SFML/Base/ThreadPool.hpp
@@ -20,25 +20,39 @@ class ThreadPool
 {
 public:
     ////////////////////////////////////////////////////////////
-    /// \brief TODO P1: docs
+    /// \brief The type of task that can be executed by the pool.
+    ///
+    /// Uses `sf::base::FixedFunction` for non-allocating storage.
     ///
     ////////////////////////////////////////////////////////////
     using Task = FixedFunction<void(), 128>;
 
     ////////////////////////////////////////////////////////////
-    /// \brief TODO P1: docs
+    /// \brief Construct a thread pool with a specified number of workers.
+    ///
+    /// Creates and starts `workerCount` threads that will wait for tasks.
+    ///
+    /// \param workerCount The number of worker threads to create.
     ///
     ////////////////////////////////////////////////////////////
     explicit ThreadPool(SizeT workerCount);
 
     ////////////////////////////////////////////////////////////
-    /// \brief TODO P1: docs
+    /// \brief Destructor.
+    ///
+    /// Waits for all currently executing and pending tasks to complete,
+    /// then stops and joins all worker threads.
     ///
     ////////////////////////////////////////////////////////////
     ~ThreadPool();
 
     ////////////////////////////////////////////////////////////
-    /// \brief TODO P1: docs
+    /// \brief Post a task to be executed by a worker thread.
+    ///
+    /// The task will be added to the queue and executed by the next
+    /// available worker thread.
+    ///
+    /// \param f The task (a callable object taking no arguments and returning void) to execute.
     ///
     ////////////////////////////////////////////////////////////
     void post(Task&& f);
@@ -50,7 +64,10 @@ public:
     [[nodiscard]] SizeT getWorkerCount() const noexcept;
 
     ////////////////////////////////////////////////////////////
-    /// \brief TODO P1: docs
+    /// \brief Get the number of concurrent threads supported by the hardware.
+    ///
+    /// Provides a hint about the optimal number of threads for CPU-bound tasks.
+    /// Equivalent to `std::thread::hardware_concurrency`.
     ///
     ////////////////////////////////////////////////////////////
     [[nodiscard]] static SizeT getHardwareWorkerCount() noexcept;
