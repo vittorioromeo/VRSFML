67735c093905d848e403a632396660384f3d1eba
vittorioromeo <vittorio.romeo@outlook.com>
2025-04-27 04:15:08 +0200

    WIP

--- include/SFML/System/AutoWrapAngle.hpp
+++ include/SFML/System/AutoWrapAngle.hpp
@@ -11,20 +11,31 @@
 namespace sf
 {
 ////////////////////////////////////////////////////////////
-/// \brief TODO P1: docs
+/// \brief A wrapper around `sf::Angle` that automatically wraps the angle value
+///
+/// This class behaves similarly to `sf::Angle` but automatically wraps
+/// the angle to the range `[0, 360)` degrees (or `[0, 2*Pi)` radians)
+/// whenever its value is accessed or used in comparisons/operations.
+///
+/// This is useful for representing properties like rotation where only
+/// the final orientation matters, regardless of the number of full turns.
 ///
 ////////////////////////////////////////////////////////////
 class [[nodiscard]] AutoWrapAngle
 {
 public:
     ////////////////////////////////////////////////////////////
-    /// \brief TODO P1: docs
+    /// \brief Default constructor
+    ///
+    /// Initializes the angle to 0 degrees.
     ///
     ////////////////////////////////////////////////////////////
     [[nodiscard]] constexpr AutoWrapAngle() = default;
 
     ////////////////////////////////////////////////////////////
-    /// \brief TODO P1: docs
+    /// \brief Construct from an `sf::Angle`
+    ///
+    /// \param angle Angle to initialize from.
     ///
     ////////////////////////////////////////////////////////////
     [[nodiscard, gnu::always_inline, gnu::flatten]] inline constexpr explicit(false) AutoWrapAngle(Angle angle) :
@@ -33,7 +44,10 @@ public:
     }
 
     ////////////////////////////////////////////////////////////
-    /// \brief TODO P1: docs
+    /// \brief Assign an `sf::Angle`
+    ///
+    /// \param angle Angle to assign.
+    /// \return Reference to `*this`.
     ///
     ////////////////////////////////////////////////////////////
     [[gnu::always_inline, gnu::flatten]] inline constexpr AutoWrapAngle& operator=(Angle angle) noexcept
@@ -43,7 +57,9 @@ public:
     }
 
     ////////////////////////////////////////////////////////////
-    /// \brief TODO P1: docs
+    /// \brief Implicit conversion to `sf::Angle`
+    ///
+    /// Returns the angle value, wrapped to the range `[0, 360)`.
     ///
     ////////////////////////////////////////////////////////////
     [[nodiscard, gnu::always_inline, gnu::flatten, gnu::pure]] inline constexpr operator Angle() const noexcept
@@ -78,7 +94,10 @@ public:
     }
 
     ////////////////////////////////////////////////////////////
-    /// \brief TODO P1: docs
+    /// \brief Rotate towards another angle by a maximum step.
+    ///
+    /// Wraps the current angle before performing the rotation.
+    /// See `sf::Angle::rotatedTowards` for details.
     ///
     ////////////////////////////////////////////////////////////
     [[nodiscard, gnu::always_inline, gnu::flatten, gnu::pure]] inline constexpr Angle rotatedTowards(Angle other, float speed) const
@@ -87,13 +106,17 @@ public:
     }
 
     ////////////////////////////////////////////////////////////
-    /// \brief TODO P1: docs
+    /// \brief Equality comparison operator
+    ///
+    /// Compares the underlying radian values without wrapping.
     ///
     ////////////////////////////////////////////////////////////
     friend constexpr bool operator==(AutoWrapAngle lhs, AutoWrapAngle rhs) = default;
 
     ////////////////////////////////////////////////////////////
-    /// \brief TODO P1: docs
+    /// \brief Addition assignment operator
+    ///
+    /// Adds `rhs` to the current angle value.
     ///
     ////////////////////////////////////////////////////////////
     [[gnu::always_inline, gnu::flatten]] inline constexpr AutoWrapAngle& operator+=(Angle rhs)
@@ -103,7 +126,9 @@ public:
     }
 
     ////////////////////////////////////////////////////////////
-    /// \brief TODO P1: docs
+    /// \brief Subtraction assignment operator
+    ///
+    /// Subtracts `rhs` from the current angle value.
     ///
     ////////////////////////////////////////////////////////////
     [[gnu::always_inline, gnu::flatten]] inline constexpr AutoWrapAngle& operator-=(Angle rhs)
@@ -113,7 +138,9 @@ public:
     }
 
     ////////////////////////////////////////////////////////////
-    /// \brief TODO P1: docs
+    /// \brief Multiplication assignment operator
+    ///
+    /// Multiplies the current angle value by `rhs`.
     ///
     ////////////////////////////////////////////////////////////
     [[gnu::always_inline, gnu::flatten]] inline constexpr AutoWrapAngle& operator*=(float rhs)
@@ -123,7 +150,9 @@ public:
     }
 
     ////////////////////////////////////////////////////////////
-    /// \brief TODO P1: docs
+    /// \brief Division assignment operator
+    ///
+    /// Divides the current angle value by `rhs`. Asserts if `rhs` is 0.
     ///
     ////////////////////////////////////////////////////////////
     [[gnu::always_inline, gnu::flatten]] inline constexpr AutoWrapAngle& operator/=(float rhs)
@@ -133,7 +162,10 @@ public:
     }
 
     ////////////////////////////////////////////////////////////
-    /// \brief TODO P1: docs
+    /// \brief Modulo assignment operator
+    ///
+    /// Assigns the result of `*this % rhs` to `*this`. Wraps the current angle
+    /// before performing the modulo operation. See `sf::Angle::operator%`.
     ///
     ////////////////////////////////////////////////////////////
     [[gnu::always_inline, gnu::flatten]] inline constexpr AutoWrapAngle& operator%=(Angle rhs)
@@ -152,6 +184,15 @@ private:
 /// \class sf::AutoWrapAngle
 /// \ingroup system
 ///
-/// TODO P1: docs
+/// `sf::AutoWrapAngle` is a wrapper around `sf::Angle` that automatically
+/// normalizes the angle to the range `[0, 360)` degrees (or `[0, 2*Pi)` radians)
+/// upon access or use in operations.
+///
+/// This is particularly useful for representing properties like rotation
+/// where angles outside the standard range are equivalent (e.g., 450 degrees
+/// is the same orientation as 90 degrees). Using `AutoWrapAngle` ensures
+/// that comparisons and operations work intuitively in such cases.
+///
+/// \see `sf::Angle`
 ///
 ////////////////////////////////////////////////////////////
--- include/SFML/System/RectUtils.hpp
+++ include/SFML/System/RectUtils.hpp
@@ -72,52 +72,11 @@ template <typename T>
 
 
 ////////////////////////////////////////////////////////////
-/// \class sf::Rect
-/// \ingroup graphics
+/// \fn `sf::findIntersection(const Rect<T>&, const Rect<T>&)`
+/// \ingroup system
 ///
-/// A rectangle is defined by its top-left corner and its size.
-/// It is a very simple class defined for convenience, so
-/// its member variables (position and size) are public
-/// and can be accessed directly, just like the vector classes
-/// (Vector2 and Vector3).
-///
-/// To keep things simple, `sf::Rect` doesn't define
-/// functions to emulate the properties that are not directly
-/// members (such as right, bottom, etc.), it rather
-/// only provides intersection functions.
-///
-/// `sf::Rect` uses the usual rules for its boundaries:
-/// \li The left and top edges are included in the rectangle's area
-/// \li The right and bottom edges are excluded from the rectangle's area
-///
-/// This means that `sf::IntRect({0, 0}, {1, 1})` and `sf::IntRect({1, 1}, {1, 1})`
-/// don't intersect.
-///
-/// sf::Rect is a template and may be used with any numeric type, but
-/// for simplicity type aliases for the instantiations used by SFML are given:
-/// \li sf::Rect<int> is sf::IntRect
-/// \li sf::Rect<float> is sf::FloatRect
-///
-/// So that you don't have to care about the template syntax.
-///
-/// Usage example:
-/// \code
-/// // Define a rectangle, located at (0, 0) with a size of 20x5
-/// sf::IntRect r1({0, 0}, {20, 5});
-///
-/// // Define another rectangle, located at (4, 2) with a size of 18x10
-/// sf::Vector2i position(4, 2);
-/// sf::Vector2i size(18, 10);
-/// sf::IntRect r2(position, size);
-///
-/// // Test intersections with the point (3, 1)
-/// bool b1 = r1.contains({3, 1}); // true
-/// bool b2 = r2.contains({3, 1}); // false
-///
-/// // Test the intersection between r1 and r2
-/// sf::base::Optional<sf::IntRect> result = r1.findIntersection(r2);
-/// // result.hasValue() == true
-/// // result.value() == sf::IntRect({4, 2}, {16, 3})
-/// \endcode
+/// Checks if two rectangles overlap and, if they do, returns the
+/// rectangle representing their intersection.
+/// Handles rectangles with negative sizes correctly.
 ///
 ////////////////////////////////////////////////////////////
