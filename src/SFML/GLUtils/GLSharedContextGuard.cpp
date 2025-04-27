d080fde486ad52f93c059a44f423530684a09782
vittorioromeo <mail@vittorioromeo.com>
2025-04-27 17:49:17 +0200

    WIP

--- src/SFML/GLUtils/GLSharedContextGuard.cpp
+++ src/SFML/GLUtils/GLSharedContextGuard.cpp
@@ -14,14 +14,22 @@
 namespace sf::priv
 {
 ////////////////////////////////////////////////////////////
-GLSharedContextGuard::GLSharedContextGuard()
+GLSharedContextGuard::GLSharedContextGuard() : m_glContext(WindowContext::getActiveThreadLocalGlContextPtr())
 {
-    if (!WindowContext::setActiveThreadLocalGlContextToSharedContext(true))
+    SFML_BASE_ASSERT(m_glContext != nullptr);
+
+    if (!WindowContext::setActiveThreadLocalGlContextToSharedContext())
         err() << "Could not enable shared GL context in `GLSharedContextGuard::GLSharedContextGuard()`";
 }
 
 
 ////////////////////////////////////////////////////////////
-GLSharedContextGuard::~GLSharedContextGuard() = default;
+GLSharedContextGuard::~GLSharedContextGuard()
+{
+    SFML_BASE_ASSERT(m_glContext != nullptr);
+
+    if (!WindowContext::setActiveThreadLocalGlContext(*m_glContext, true))
+        err() << "Could not restore context in `GLSharedContextGuard::~GLSharedContextGuard()`";
+}
 
 } // namespace sf::priv
