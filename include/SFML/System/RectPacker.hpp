6d3452b2439efc4486bd183c0b18c0338d2ca65a
vittorioromeo <vittorio.romeo@outlook.com>
2025-04-27 04:15:08 +0200

    WIP

--- include/SFML/System/RectPacker.hpp
+++ include/SFML/System/RectPacker.hpp
@@ -14,32 +14,39 @@
 namespace sf
 {
 ////////////////////////////////////////////////////////////
-/// \brief TODO P1: docs
+/// \brief Packs 2D rectangles into a larger texture area efficiently.
+///
+/// This class implements an algorithm to find positions for smaller
+/// rectangles within a larger, fixed-size area (bin). It's commonly used
+/// for creating texture atlases, where multiple smaller images are packed
+/// into a single larger texture to optimize rendering.
 ///
 ////////////////////////////////////////////////////////////
 class RectPacker
 {
 public:
     ////////////////////////////////////////////////////////////
-    /// \brief TODO P1: docs
+    /// \brief Construct a rectangle packer for a given bin size.
+    ///
+    /// \param size The dimensions (width, height) of the area to pack rectangles into.
     ///
     ////////////////////////////////////////////////////////////
     [[nodiscard]] explicit RectPacker(Vector2u size);
 
     ////////////////////////////////////////////////////////////
-    /// \brief TODO P1: docs
+    /// \brief Destructor
     ///
     ////////////////////////////////////////////////////////////
     ~RectPacker();
 
     ////////////////////////////////////////////////////////////
-    /// \brief Copy constructor
+    /// \brief Deleted copy constructor
     ///
     ////////////////////////////////////////////////////////////
     RectPacker(const RectPacker& rhs) = delete;
 
     ////////////////////////////////////////////////////////////
-    /// \brief Copy assignment operator
+    /// \brief Deleted copy assignment operator
     ///
     ////////////////////////////////////////////////////////////
     RectPacker& operator=(const RectPacker& rhs) = delete;
@@ -57,14 +64,24 @@ public:
     RectPacker& operator=(RectPacker&& rhs) noexcept;
 
     ////////////////////////////////////////////////////////////
-    /// \brief TODO P1: docs
+    /// \brief Attempt to pack a rectangle of the given size.
+    ///
+    /// Tries to find an available space within the bin for a rectangle
+    /// with the specified dimensions.
+    ///
+    /// \param rectSize The size (width, height) of the rectangle to pack.
+    ///                 Both dimensions must be greater than zero.
+    ///
+    /// \return An `Optional<Vector2u>` containing the top-left position
+    ///         where the rectangle was packed if successful, or `nullOpt`
+    ///         if there was no room left in the bin.
     ///
     ////////////////////////////////////////////////////////////
     // TODO P1: add overload to pack multiple rects at once
     [[nodiscard]] base::Optional<Vector2u> pack(Vector2u rectSize);
 
     ////////////////////////////////////////////////////////////
-    /// \brief TODO P1: docs
+    /// \brief Get the size of the packing area (the bin).
     ///
     ////////////////////////////////////////////////////////////
     [[nodiscard]] Vector2u getSize() const;
@@ -84,7 +101,15 @@ private:
 /// \class sf::RectPacker
 /// \ingroup system
 ///
-/// TODO P1: docs
+/// `sf::RectPacker` helps arrange smaller rectangular areas within a larger
+/// one without overlapping. This is useful for tasks like creating texture atlases,
+/// where you want to combine multiple small images into a single larger texture
+/// to improve performance.
+///
+/// You create a `RectPacker` with the dimensions of the target area (the "bin").
+/// Then, you repeatedly call `pack()` with the dimensions of the rectangles you
+/// want to place. `pack()` returns the top-left position within the bin where
+/// the rectangle was placed, or `nullOpt` if it couldn't fit.
 ///
 /// \see sf::Rect
 ///
