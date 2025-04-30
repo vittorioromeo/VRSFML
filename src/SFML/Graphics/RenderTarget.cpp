b5e8a135fc15f3ee06c8401414e010d2ac9398bb
vittorioromeo <mail@vittorioromeo.com>
2025-05-01 01:08:02 +0200

    WIP

--- src/SFML/Graphics/RenderTarget.cpp
+++ src/SFML/Graphics/RenderTarget.cpp
@@ -161,50 +161,36 @@ SFML_PRIV_DEFINE_ENUM_TO_GLENUM_CONVERSION_FN(
 
 
 ////////////////////////////////////////////////////////////
-enum : bool
-{
-#ifdef SFML_OPENGL_ES
-    isOpenGLES = true
-#else
-    isOpenGLES = false
-#endif
-};
-
-
-////////////////////////////////////////////////////////////
 [[gnu::always_inline, gnu::flatten]] inline void streamToGPU(const unsigned int    bufferId,
                                                              const void* const     data,
                                                              const sf::base::SizeT dataByteCount)
 {
-    if constexpr (isOpenGLES)
+#ifdef SFML_OPENGL_ES
+    // On OpenGL ES, the "naive" method seems faster, also named buffers are not supported
+    glCheck(glBufferData(bufferId, static_cast<GLsizeiptr>(dataByteCount), data, GL_STREAM_DRAW));
+#else
+    // For small batches, the "naive" method also seems faster
+    if (dataByteCount < sizeof(sf::Vertex) * 64u)
     {
-        // On OpenGL ES, the "naive" method seems faster, also named buffers are not supported
-        glCheck(glBufferData(bufferId, static_cast<GLsizeiptr>(dataByteCount), data, GL_STREAM_DRAW));
+        glCheck(glNamedBufferData(bufferId, static_cast<GLsizeiptr>(dataByteCount), nullptr, GL_STREAM_DRAW)); // Must orphan first
+        glCheck(glNamedBufferData(bufferId, static_cast<GLsizeiptr>(dataByteCount), data, GL_STREAM_DRAW));
+        return;
     }
-    else
-    {
-        // For small batches, the "naive" method also seems faster
-        if (dataByteCount < sizeof(sf::Vertex) * 64u)
-        {
-            glCheck(glNamedBufferData(bufferId, static_cast<GLsizeiptr>(dataByteCount), nullptr, GL_STREAM_DRAW)); // Must orphan first
-            glCheck(glNamedBufferData(bufferId, static_cast<GLsizeiptr>(dataByteCount), data, GL_STREAM_DRAW));
-            return;
-        }
 
-        // For larger batches, memcpying into a transient mapped buffer seems faster
-        glCheck(glNamedBufferData(bufferId, static_cast<GLsizeiptr>(dataByteCount), nullptr, GL_STREAM_DRAW));
+    // For larger batches, memcpying into a transient mapped buffer seems faster
+    glCheck(glNamedBufferData(bufferId, static_cast<GLsizeiptr>(dataByteCount), nullptr, GL_STREAM_DRAW));
 
-        void* const ptr = glCheck(
-            glMapNamedBufferRange(bufferId,
-                                  0u,
-                                  static_cast<GLsizeiptr>(dataByteCount),
-                                  GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT | GL_MAP_INVALIDATE_RANGE_BIT));
+    void* const ptr = glCheck(
+        glMapNamedBufferRange(bufferId,
+                              0u,
+                              static_cast<GLsizeiptr>(dataByteCount),
+                              GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT | GL_MAP_INVALIDATE_RANGE_BIT));
 
-        SFML_BASE_MEMCPY(ptr, data, dataByteCount);
+    SFML_BASE_MEMCPY(ptr, data, dataByteCount);
 
-        [[maybe_unused]] const auto rc = glCheck(glUnmapNamedBuffer(bufferId));
-        SFML_BASE_ASSERT(rc == GL_TRUE);
-    }
+    [[maybe_unused]] const auto rc = glCheck(glUnmapNamedBuffer(bufferId));
+    SFML_BASE_ASSERT(rc == GL_TRUE);
+#endif
 }
 
 
@@ -213,7 +199,14 @@ enum : bool
                                                                      const sf::Vertex*                   vertexData,
                                                                      const sf::base::SizeT               vertexCount)
 {
-    streamToGPU(isOpenGLES ? GL_ARRAY_BUFFER : bufferId, vertexData, sizeof(sf::Vertex) * vertexCount);
+    streamToGPU(
+#ifdef SFML_OPENGL_ES
+        GL_ARRAY_BUFFER,
+#else
+        bufferId,
+#endif
+        vertexData,
+        sizeof(sf::Vertex) * vertexCount);
 }
 
 
@@ -222,7 +215,14 @@ enum : bool
                                                                     const sf::IndexType*                indexData,
                                                                     const sf::base::SizeT               indexCount)
 {
-    streamToGPU(isOpenGLES ? GL_ELEMENT_ARRAY_BUFFER : bufferId, indexData, sizeof(sf::IndexType) * indexCount);
+    streamToGPU(
+#ifdef SFML_OPENGL_ES
+        GL_ELEMENT_ARRAY_BUFFER,
+#else
+        bufferId,
+#endif
+        indexData,
+        sizeof(sf::IndexType) * indexCount);
 }
 
 
