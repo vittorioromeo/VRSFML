3a9e22498c59529e2321e01c2f3ae1b83f5afc31
vittorioromeo <mail@vittorioromeo.com>
2025-05-05 16:30:36 +0200

    More shape goodness

---
+++ include/SFML/Graphics/RingShapeData.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include <SFML/Copyright.hpp> // LICENSE AND COPYRIGHT (C) INFORMATION
+
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include "SFML/Graphics/Export.hpp"
+
+#include "SFML/Graphics/Priv/ShapeMacros.hpp"
+#include "SFML/Graphics/Priv/TransformableMacros.hpp"
+
+
+namespace sf
+{
+////////////////////////////////////////////////////////////
+/// \brief Data required to draw a ring shape
+///
+////////////////////////////////////////////////////////////
+struct [[nodiscard]] SFML_GRAPHICS_API RingShapeData
+{
+    SFML_PRIV_DEFINE_SETTINGS_DATA_MEMBERS_TRANSFORMABLE;
+    SFML_PRIV_DEFINE_SETTINGS_DATA_MEMBERS_SHAPE;
+
+    float        outerRadius{0.f}; //!< Distance from the center to the outer points
+    float        innerRadius{0.f}; //!< Distance from the center to the inner points
+    unsigned int pointCount{30u};  //!< Number of points composing the ring
+};
+
+} // namespace sf
+
+
+////////////////////////////////////////////////////////////
+/// \class sf::RingShapeData
+/// \ingroup graphics
+///
+/// TODO P1: docs
+///
+/// \see `sf::Shape`, `sf::CircleShape`
+///
+////////////////////////////////////////////////////////////
