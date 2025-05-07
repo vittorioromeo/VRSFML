2fb8d035f1c14f209de65ea7d1d78502fbdd3c8e
vittorioromeo <mail@vittorioromeo.com>
2025-05-07 17:32:14 +0200

    WIP

--- test/TestUtilities/StringifyStringUtil.hpp
+++ test/TestUtilities/StringifyStringUtil.hpp
@@ -12,7 +12,7 @@ struct StringMaker<char8_t>
 {
     static doctest::String convert(const char8_t&)
     {
-        return ""; // TODO P2:
+        return ""; // TODO P3:
     }
 };
 
@@ -21,7 +21,7 @@ struct StringMaker<char16_t>
 {
     static doctest::String convert(const char16_t&)
     {
-        return ""; // TODO P2:
+        return ""; // TODO P3:
     }
 };
 
@@ -30,7 +30,7 @@ struct StringMaker<std::basic_string<T>>
 {
     static doctest::String convert(const std::basic_string<T>&)
     {
-        return ""; // TODO P2:
+        return ""; // TODO P3:
     }
 };
 
@@ -39,7 +39,7 @@ struct StringMaker<std::basic_string_view<T>>
 {
     static doctest::String convert(const std::basic_string_view<T>&)
     {
-        return ""; // TODO P2:
+        return ""; // TODO P3:
     }
 };
 
