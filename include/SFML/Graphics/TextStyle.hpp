01341807268487201b87f4ae737b1462159ec62e
vittorioromeo <mail@vittorioromeo.com>
2025-05-25 18:00:02 +0200

    WIP

---
+++ include/SFML/Graphics/TextStyle.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <SFML/Copyright.hpp> // LICENSE AND COPYRIGHT (C) INFORMATION
+
+
+namespace sf
+{
+////////////////////////////////////////////////////////////
+/// \brief Enumeration of text drawing styles
+///
+////////////////////////////////////////////////////////////
+enum class [[nodiscard]] TextStyle : unsigned char
+{
+    Regular       = 0,      //!< Regular characters, no style
+    Bold          = 1 << 0, //!< Bold characters
+    Italic        = 1 << 1, //!< Italic characters
+    Underlined    = 1 << 2, //!< Underlined characters
+    StrikeThrough = 1 << 3  //!< Strike through characters
+};
+
+
+} // namespace sf
