f7c1f4522d0465cd023392aa0857d8c0150cc31f
vittorioromeo <vittorio.romeo@outlook.com>
2025-10-11 00:08:11 +0200

    WIP

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 basepwd=$(basename $PWD)
-ninja $1 && (cd ../test && ../$basepwd/bin/$1* "${@:2}")
+lowercase_arg=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+ninja "test-sfml-$lowercase_arg" && (cd "../test/$1" && ../../$basepwd/bin/test-sfml-$lowercase_arg* "${@:2}")
