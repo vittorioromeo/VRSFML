d34cdb19049004449e77dfe7ddeef1d2016c2c69
vittorioromeo <mail@vittorioromeo.com>
2025-10-21 17:22:06 +0200

    SRS

--- examples/tetris_survivors/Block.hpp
+++ examples/tetris_survivors/Block.hpp
@@ -10,32 +10,29 @@
 #include "SFML/Graphics/Color.hpp"
 
 #include "SFML/Base/Array.hpp"
-#include "SFML/Base/Assert.hpp"
-#include "SFML/Base/SizeT.hpp"
-#include "SFML/Base/Swap.hpp"
-#include "SFML/Base/Vector.hpp"
+#include "SFML/Base/IntTypes.hpp"
 
 
 namespace tsurv
 {
 ////////////////////////////////////////////////////////////
-inline constexpr sf::base::Array<sf::Color, tetraminoShapeCount> blockPalette = {
-    sf::Color(13, 194, 255), // I - Cyan
-    sf::Color(255, 225, 56), // O - Yellow
-    sf::Color(245, 56, 255), // T - Purple
-    sf::Color(255, 142, 13), // L - Orange
-    sf::Color(56, 119, 255), // J - Blue
-    sf::Color(13, 255, 114), // S - Green
-    sf::Color(255, 13, 114)  // Z - Red
+inline constexpr sf::base::Array<sf::Color, tetraminoShapeCount> blockPalette{
+    sf::Color{13u, 194u, 255u}, // I - Cyan
+    sf::Color{255u, 225u, 56u}, // O - Yellow
+    sf::Color{245u, 56u, 255u}, // T - Purple
+    sf::Color{255u, 142u, 13u}, // L - Orange
+    sf::Color{56u, 119u, 255u}, // J - Blue
+    sf::Color{13u, 255u, 114u}, // S - Green
+    sf::Color{255u, 13u, 114u}  // Z - Red
 };
 
 
 ////////////////////////////////////////////////////////////
 struct Block
 {
-    int             health;
-    sf::base::SizeT paletteIdx;
-    ShapeBlock      shapeBlock;
+    sf::base::U8 health;
+    sf::base::U8 paletteIdx;
+    ShapeBlock   shapeBlock;
 };
 
 } // namespace tsurv
--- include/SFML/ImGui/ImConfigSFML.hpp
+++ include/SFML/ImGui/ImConfigSFML.hpp
@@ -19,31 +19,31 @@
 ////////////////////////////////////////////////////////////
 #define IM_VEC2_CLASS_EXTRA                                         \
     template <typename T>                                           \
-    ImVec2(const ::sf::Vec2<T>& v)                                  \
+    explicit ImVec2(const ::sf::Vec2<T>& v)                         \
     {                                                               \
         x = static_cast<float>(v.x);                                \
         y = static_cast<float>(v.y);                                \
     }                                                               \
                                                                     \
     template <typename T>                                           \
-    operator ::sf::Vec2<T>() const                                  \
+    explicit operator ::sf::Vec2<T>() const                         \
     {                                                               \
         return ::sf::Vec2<T>(static_cast<T>(x), static_cast<T>(y)); \
     }
 
 
 ////////////////////////////////////////////////////////////
-#define IM_VEC4_CLASS_EXTRA                                                                       \
-    ImVec4(const ::sf::Color& c) : x(c.r / 255.f), y(c.g / 255.f), z(c.b / 255.f), w(c.a / 255.f) \
-    {                                                                                             \
-    }                                                                                             \
-                                                                                                  \
-    operator ::sf::Color() const                                                                  \
-    {                                                                                             \
-        return ::sf::Color(static_cast<::sf::base::U8>(x * 255.f),                                \
-                           static_cast<::sf::base::U8>(y * 255.f),                                \
-                           static_cast<::sf::base::U8>(z * 255.f),                                \
-                           static_cast<::sf::base::U8>(w * 255.f));                               \
+#define IM_VEC4_CLASS_EXTRA                                                                                \
+    explicit ImVec4(const ::sf::Color& c) : x(c.r / 255.f), y(c.g / 255.f), z(c.b / 255.f), w(c.a / 255.f) \
+    {                                                                                                      \
+    }                                                                                                      \
+                                                                                                           \
+    operator ::sf::Color() const                                                                           \
+    {                                                                                                      \
+        return ::sf::Color(static_cast<::sf::base::U8>(x * 255.f),                                         \
+                           static_cast<::sf::base::U8>(y * 255.f),                                         \
+                           static_cast<::sf::base::U8>(z * 255.f),                                         \
+                           static_cast<::sf::base::U8>(w * 255.f));                                        \
     }
 
 
