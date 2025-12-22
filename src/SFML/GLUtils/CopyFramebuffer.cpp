3b7b9d1a15b4bb50b63a70ec71a33aa89cebfcbe
Vittorio Romeo <mail@vittorioromeo.com>
2025-12-22 15:43:48 +0100

    Minor header cleanup

---
+++ src/SFML/GLUtils/CopyFramebuffer.cpp
@@ -0,0 +1,83 @@
+// LICENSE AND COPYRIGHT (C) INFORMATION
+// https://github.com/vittorioromeo/VRSFML/blob/master/license.md
+
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include "SFML/GLUtils/CopyFramebuffer.hpp"
+
+#include "SFML/Config.hpp"
+
+#include "SFML/GLUtils/BlitFramebuffer.hpp"
+#include "SFML/GLUtils/FramebufferSaver.hpp"
+#include "SFML/GLUtils/GLCheck.hpp"
+#include "SFML/GLUtils/Glad.hpp"
+
+#ifdef SFML_OPENGL_ES
+    #include "SFML/System/Err.hpp"
+#endif
+
+
+namespace sf::priv
+{
+////////////////////////////////////////////////////////////
+void copyFramebuffer(const bool         invertYAxis,
+                     const Vec2u        size,
+                     const unsigned int srcFBO,
+                     const unsigned int dstFBO,
+                     const Vec2u        srcPos,
+                     const Vec2u        dstPos)
+{
+    glCheck(glBindFramebuffer(GL_READ_FRAMEBUFFER, srcFBO));
+    glCheck(glBindFramebuffer(GL_DRAW_FRAMEBUFFER, dstFBO));
+
+    SFML_BASE_ASSERT(glCheck(glCheckFramebufferStatus(GL_READ_FRAMEBUFFER)) == GL_FRAMEBUFFER_COMPLETE);
+    SFML_BASE_ASSERT(glCheck(glCheckFramebufferStatus(GL_DRAW_FRAMEBUFFER)) == GL_FRAMEBUFFER_COMPLETE);
+
+    blitFramebuffer(invertYAxis, size, srcPos, dstPos);
+}
+
+
+////////////////////////////////////////////////////////////
+bool copyFlippedFramebuffer([[maybe_unused]] const unsigned int tmpTextureNativeHandle,
+                            const Vec2u                         size,
+                            const unsigned int                  srcFBO,
+                            const unsigned int                  dstFBO,
+                            const Vec2u                         srcPos,
+                            const Vec2u                         dstPos)
+{
+    const FramebufferSaver framebufferSaver;
+
+#ifndef SFML_OPENGL_ES
+
+    priv::copyFramebuffer(/* invertYAxis */ true, size, srcFBO, dstFBO, srcPos, dstPos);
+    return true;
+
+#else
+
+    const GLuint intermediateFBO = priv::generateAndBindFramebuffer();
+    if (!intermediateFBO)
+    {
+        priv::err() << "Failure to create intermediate FBO in `copyFlippedFramebuffer`";
+        return false;
+    }
+
+    glCheck(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, tmpTextureNativeHandle, 0));
+
+    if (glCheck(glCheckFramebufferStatus(GL_FRAMEBUFFER)) != GL_FRAMEBUFFER_COMPLETE)
+    {
+        priv::err() << "Failure to complete intermediate FBO in `copyFlippedFramebuffer`";
+        return false;
+    }
+
+    priv::copyFramebuffer(/* invertYAxis */ false, size, srcFBO, intermediateFBO, srcPos, dstPos);
+    priv::copyFramebuffer(/* invertYAxis */ true, size, intermediateFBO, dstFBO, srcPos, dstPos);
+
+    glCheck(glDeleteFramebuffers(1, &intermediateFBO));
+    return true;
+
+#endif
+}
+
+} // namespace sf::priv
---
+++ src/SFML/GLUtils/CopyFramebuffer.hpp
@@ -0,0 +1,59 @@
+#pragma once
+// LICENSE AND COPYRIGHT (C) INFORMATION
+// https://github.com/vittorioromeo/VRSFML/blob/master/license.md
+
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include "SFML/System/Vec2.hpp"
+
+
+namespace sf::priv
+{
+////////////////////////////////////////////////////////////
+/// \brief Copy framebuffer contents between specified FBOs
+///
+/// Binds the given source and destination framebuffer objects (FBOs) and performs
+/// a blit operation between them with optional vertical flipping.
+///
+/// \param invertYAxis Whether to flip the source vertically during copy
+/// \param size        Dimensions of the region to copy
+/// \param srcFBO      Source framebuffer ID
+/// \param dstFBO      Destination framebuffer ID
+/// \param srcPos      Source region starting position (default: 0,0)
+/// \param dstPos      Destination region starting position (default: 0,0)
+///
+////////////////////////////////////////////////////////////
+void copyFramebuffer(bool         invertYAxis,
+                     Vec2u        size,
+                     unsigned int srcFBO,
+                     unsigned int dstFBO,
+                     Vec2u        srcPos = {0u, 0u},
+                     Vec2u        dstPos = {0u, 0u});
+
+////////////////////////////////////////////////////////////
+/// \brief Copy framebuffer contents with vertical flipping
+///
+/// Copies source framebuffer contents to destination while vertically flipping
+/// the image. Uses an intermediate framebuffer for OpenGL ES compatibility.
+///
+/// \param tmpTextureNativeHandle Temporary texture handle for intermediate operations (OpenGL ES only)
+/// \param size                   Dimensions of the region to copy
+/// \param srcFBO                 Source framebuffer ID
+/// \param dstFBO                 Destination framebuffer ID
+/// \param srcPos                 Source region starting position (default: 0,0)
+/// \param dstPos                 Destination region starting position (default: 0,0)
+///
+/// \return True if copy succeeded, false otherwise
+///
+////////////////////////////////////////////////////////////
+[[nodiscard]] bool copyFlippedFramebuffer(
+    unsigned int tmpTextureNativeHandle,
+    Vec2u        size,
+    unsigned int srcFBO,
+    unsigned int dstFBO,
+    Vec2u        srcPos = {0u, 0u},
+    Vec2u        dstPos = {0u, 0u});
+
+} // namespace sf::priv
