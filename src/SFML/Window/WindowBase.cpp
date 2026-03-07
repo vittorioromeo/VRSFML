0c2a15904bfd8a384796b4491938f01b66a7f4ab
vittorioromeo <vittorio.romeo@outlook.com>
2026-03-07 17:13:31 +0100

    Add SourceLocation, fix Vulkan

--- src/SFML/Window/WindowBase.cpp
+++ src/SFML/Window/WindowBase.cpp
@@ -38,7 +38,7 @@ namespace
         return nullptr;
 
     windowSettings.contextSettings.antiAliasingLevel /= 2u;
-    return  (windowSettings);
+    return tryCreateSDLWindowImpl(windowSettings);
 }
 
 } // namespace
