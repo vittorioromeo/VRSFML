a136baa7b2fd717312a1729e277b2059d8d869a2
Vittorio Romeo <mail@vittorioromeo.com>
2026-03-27 01:10:43 +0100

    Add some vector operations to ImConfigSFML

--- include/SFML/ImGui/ImConfigSFML.hpp
+++ include/SFML/ImGui/ImConfigSFML.hpp
@@ -6,31 +6,43 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
-#include "SFML/ImGui/Export.hpp"
+#include "SFML/Graphics/Color.hpp" // IWYU pragma: keep
 
-#include "SFML/Graphics/Color.hpp"
-
-#include "SFML/System/Vec2Base.hpp"
+#include "SFML/System/Vec2Base.hpp" // IWYU pragma: keep
 
 #include "SFML/Base/Assert.hpp"
-#include "SFML/Base/IntTypes.hpp"
+#include "SFML/Base/IntTypes.hpp" // IWYU pragma: keep
 
 
 ////////////////////////////////////////////////////////////
-#define IM_VEC2_CLASS_EXTRA                                         \
-    template <typename T>                                           \
-    explicit ImVec2(const ::sf::Vec2<T>& v)                         \
-    {                                                               \
-        x = static_cast<float>(v.x);                                \
-        y = static_cast<float>(v.y);                                \
-    }                                                               \
-                                                                    \
-    template <typename T>                                           \
-    explicit operator ::sf::Vec2<T>() const                         \
-    {                                                               \
-        return ::sf::Vec2<T>(static_cast<T>(x), static_cast<T>(y)); \
+#define IM_VEC2_CLASS_EXTRA                                                        \
+    template <typename T>                                                          \
+    explicit ImVec2(const ::sf::Vec2<T>& v)                                        \
+    {                                                                              \
+        x = static_cast<float>(v.x);                                               \
+        y = static_cast<float>(v.y);                                               \
+    }                                                                              \
+                                                                                   \
+    template <typename T>                                                          \
+    [[gnu::always_inline, gnu::pure]] explicit operator ::sf::Vec2<T>() const      \
+    {                                                                              \
+        return ::sf::Vec2<T>(static_cast<T>(x), static_cast<T>(y));                \
+    }                                                                              \
+                                                                                   \
+    [[gnu::always_inline, gnu::pure]] operator ::sf::Vec2f() const                 \
+    {                                                                              \
+        return ::sf::Vec2f(x, y);                                                  \
+    }                                                                              \
+                                                                                   \
+    [[gnu::always_inline, gnu::pure]] ImVec2 operator+(const ::sf::Vec2f& v) const \
+    {                                                                              \
+        return ImVec2(x + v.x, y + v.y);                                           \
+    }                                                                              \
+                                                                                   \
+    [[gnu::always_inline, gnu::pure]] ImVec2 operator-(const ::sf::Vec2f& v) const \
+    {                                                                              \
+        return ImVec2(x - v.x, y - v.y);                                           \
     }
-
 
 ////////////////////////////////////////////////////////////
 #define IM_VEC4_CLASS_EXTRA                                                                                \
