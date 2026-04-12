10a18bb74becf4321c3efecbb3267292e135fb13
Vittorio Romeo <mail@vittorioromeo.com>
2026-04-12 22:38:43 +0200

    Improve emscripten scripts

--- run_emscripten_example_rel.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-killall python3
-(cd $SCRIPT_DIR/build_vrdev_emcc_rel && ninja $1 && emrun ./bin/$1.html &)
