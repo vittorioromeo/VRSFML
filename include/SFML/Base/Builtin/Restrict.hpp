496f55bb1b7332100e745dda4b441df1cd0233c2
Vittorio Romeo <mail@vittorioromeo.com>
2026-05-11 15:10:40 +0200

    Docs consistency

--- include/SFML/Base/Builtin/Restrict.hpp
+++ include/SFML/Base/Builtin/Restrict.hpp
@@ -3,17 +3,6 @@
 // https://github.com/vittorioromeo/VRSFML/blob/master/license.md
 
 
-////////////////////////////////////////////////////////////
-/// \file
-/// \brief Portable wrapper for the C99 `restrict` qualifier
-///
-/// Tells the compiler that two pointer parameters do not alias each
-/// other, which can unlock vectorization. Expands to `__restrict__`
-/// on GCC/Clang, `__restrict` on MSVC, and nothing elsewhere.
-///
-////////////////////////////////////////////////////////////
-
-
 #if defined(__GNUC__) || defined(__clang__)
 
     ////////////////////////////////////////////////////////////
@@ -30,3 +19,14 @@
     #define SFML_BASE_RESTRICT
 
 #endif
+
+
+////////////////////////////////////////////////////////////
+/// \file
+/// \brief Portable wrapper for the C99 `restrict` qualifier
+///
+/// Tells the compiler that two pointer parameters do not alias each
+/// other, which can unlock vectorization. Expands to `__restrict__`
+/// on GCC/Clang, `__restrict` on MSVC, and nothing elsewhere.
+///
+////////////////////////////////////////////////////////////
