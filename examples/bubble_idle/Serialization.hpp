8ddce095646a107b66d4c9f455e3f8ff91159d8a
Vittorio Romeo <mail@vittorioromeo.com>
2026-05-15 00:51:16 +0200

    Compile time opts

--- examples/bubble_idle/Serialization.hpp
+++ examples/bubble_idle/Serialization.hpp
@@ -15,8 +15,8 @@ struct GameConstants;
 
 ////////////////////////////////////////////////////////////
 bool saveProfileToFile(const Profile& profile, const char* filename = "userdata/profile.json");
-void loadProfileFromFile(Profile& profile, const char* filename = "userdata/profile.json");
+bool loadProfileFromFile(Profile& profile, const char* filename = "userdata/profile.json");
 bool saveGameConstantsToFile(const GameConstants& gameConstants, const char* filename = "resources/game_constants.json");
-void loadGameConstantsFromFile(GameConstants& gameConstants, const char* filename = "resources/game_constants.json");
+bool loadGameConstantsFromFile(GameConstants& gameConstants, const char* filename = "resources/game_constants.json");
 bool savePlaythroughToFile(const Playthrough& playthrough, const char* filename);
 [[nodiscard]] sf::base::StringView loadPlaythroughFromFile(Playthrough& playthrough, const char* filename);
