9bdac0d64d208f8e034b06fbf70997b823182c8f
Vittorio Romeo <mail@vittorioromeo.com>
2026-05-27 10:31:32 +0200

    include/SFML/Base/NonDeduced.hpp -- NonDeduced<T> template wrapper to suppress argument deduction
    include/SFML/Base/Trait/IsAssignable.hpp -- isAssignable<T,U> trait + SFML_BASE_IS_ASSIGNABLE macro
    
    include/SFML/Base/Builtin/Pragma.hpp -- SFML_BASE_PRAGMA(...) macro for statement-level pragmas

---
+++ include/SFML/Base/Builtin/Pragma.hpp
@@ -0,0 +1,7 @@
+#pragma once
+// LICENSE AND COPYRIGHT (C) INFORMATION
+// https://github.com/vittorioromeo/VRSFML/blob/master/license.md
+
+
+////////////////////////////////////////////////////////////
+#define SFML_BASE_PRAGMA(...) _Pragma(#__VA_ARGS__) static_assert(true)
